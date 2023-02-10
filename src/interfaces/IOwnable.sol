4fbe267f2a01ff5f4e0309d3b2be41aa22fafef8
Iain <iain@zora.co>
2023-02-10 13:23:15 -0500

    add ownable functionality

---
+++ src/interfaces/IOwnable.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IOwnable {
+    function owner() external returns (address);
+
+    event OwnershipTransferred(address lastOwner, address newOwner);
+}
