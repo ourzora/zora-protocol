316b73e253f65353b7e2795aaacff844d50a4de2
tbtstl <tysonbattistella@gmail.com>
2023-02-22 16:03:43 -0500

    [chore] add clarifying RoyaltyConfiguration comment

--- src/interfaces/ICreatorRoyaltiesControl.sol
+++ src/interfaces/ICreatorRoyaltiesControl.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.17;
 import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 interface ICreatorRoyaltiesControl is IERC2981 {
+    /// @notice The RoyaltyConfiguration struct is used to store the royalty configuration for a given token.
+    /// @param royaltyMintSchedule 1/N tokens are minted to the royalty recipient
+    /// @param royaltyBPS The royalty amount in basis points for secondary sales.
+    /// @param royaltyRecipient The address that will receive the royalty payments.
     struct RoyaltyConfiguration {
         uint32 royaltyMintSchedule;
         uint32 royaltyBPS;
