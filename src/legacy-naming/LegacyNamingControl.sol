4cbab763751dd504c234a097958b01e3a79b5745
tbtstl <tysonbattistella@gmail.com>
2023-03-03 20:30:46 -0700

    [chore] natspec LegacyNamingControl

--- src/legacy-naming/LegacyNamingControl.sol
+++ src/legacy-naming/LegacyNamingControl.sol
@@ -4,11 +4,15 @@ pragma solidity 0.8.17;
 import {ILegacyNaming} from "../interfaces/ILegacyNaming.sol";
 import {LegacyNamingStorageV1} from "./LegacyNamingStorageV1.sol";
 
+/// @title LegacyNamingControl
+/// @notice Contract for managing the name and symbol of an 1155 contract in the legacy naming scheme
 contract LegacyNamingControl is LegacyNamingStorageV1, ILegacyNaming {
+    /// @notice The name of the contract
     function name() external view returns (string memory) {
-      return _name;
+        return _name;
     }
 
+    /// @notice The token symbol of the contract
     function symbol() external pure returns (string memory) {}
 
     function _setName(string memory _newName) internal {
