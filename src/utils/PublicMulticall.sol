79f09ce2aa31f5384dabee1bc50d1dd1d248390e
tbtstl <tysonbattistella@gmail.com>
2023-03-03 21:19:34 -0700

    [chore] natspec PublicMulticall

--- src/utils/PublicMulticall.sol
+++ src/utils/PublicMulticall.sol
@@ -3,15 +3,13 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 
+/// @title PublicMulticall
+/// @notice Contract for executing a batch of function calls on this contract
 abstract contract PublicMulticall {
     /**
-     * @dev Receives and executes a batch of function calls on this contract.
+     * @notice Receives and executes a batch of function calls on this contract.
      */
-    function multicall(bytes[] calldata data)
-        public
-        virtual
-        returns (bytes[] memory results)
-    {
+    function multicall(bytes[] calldata data) public virtual returns (bytes[] memory results) {
         results = new bytes[](data.length);
         for (uint256 i = 0; i < data.length; i++) {
             results[i] = Address.functionDelegateCall(address(this), data[i]);
