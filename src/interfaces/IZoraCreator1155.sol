4b8311e62f386d42245c543bfd685abb27116d7e
Iain <iain@zora.co>
2023-03-15 18:00:32 -0400

    add permission control docs

--- src/interfaces/IZoraCreator1155.sol
+++ src/interfaces/IZoraCreator1155.sol
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {IERC165Upgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC165Upgradeable.sol";
+import {IERC1155MetadataURIUpgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC1155MetadataURIUpgradeable.sol";
 import {IZoraCreator1155TypesV1} from "../nft/IZoraCreator1155TypesV1.sol";
 import {IRenderer1155} from "../interfaces/IRenderer1155.sol";
 import {IMinter1155} from "../interfaces/IMinter1155.sol";
 import {IVersionedContract} from "./IVersionedContract.sol";
 import {ICreatorRoyaltiesControl} from "../interfaces/ICreatorRoyaltiesControl.sol";
 
-interface IZoraCreator1155 is IZoraCreator1155TypesV1, IVersionedContract {
+
+/// @notice Main interface for the ZoraCreator1155 contract
+interface IZoraCreator1155 is IZoraCreator1155TypesV1, IVersionedContract, IERC1155MetadataURIUpgradeable {
     function PERMISSION_BIT_ADMIN() external returns (uint256);
 
     function PERMISSION_BIT_MINTER() external returns (uint256);
@@ -40,8 +44,7 @@ interface IZoraCreator1155 is IZoraCreator1155TypesV1, IVersionedContract {
     error ETHWithdrawFailed(address recipient, uint256 amount);
     error FundsWithdrawInsolvent(uint256 amount, uint256 contractValue);
 
-    // TODO: maybe add more context
-    error CannotMintMoreTokens(uint256 tokenId);
+    error CannotMintMoreTokens(uint256 tokenId, uint256 quantity, uint256 totalMinted, uint256 maxSupply);
 
     function initialize(
         string memory contractURI,
@@ -56,20 +59,53 @@ interface IZoraCreator1155 is IZoraCreator1155TypesV1, IVersionedContract {
     /// @param tokenId tokenId to mint, set to 0 for new tokenId
     /// @param quantity to purchase
     /// @param minterArguments calldata for the minter contracts
-    function purchase(IMinter1155 minter, uint256 tokenId, uint256 quantity, bytes calldata minterArguments) external payable;
+    function purchase(
+        IMinter1155 minter,
+        uint256 tokenId,
+        uint256 quantity,
+        bytes calldata minterArguments
+    ) external payable;
 
-    function adminMint(address recipient, uint256 tokenId, uint256 quantity, bytes memory data) external;
+    function adminMint(
+        address recipient,
+        uint256 tokenId,
+        uint256 quantity,
+        bytes memory data
+    ) external;
 
-    function adminMintBatch(address recipient, uint256[] memory tokenIds, uint256[] memory quantities, bytes memory data) external;
+    function adminMintBatch(
+        address recipient,
+        uint256[] memory tokenIds,
+        uint256[] memory quantities,
+        bytes memory data
+    ) external;
 
     /// @notice Contract call to setupNewToken
     /// @param _uri URI for the token
     /// @param maxSupply maxSupply for the token, set to 0 for open edition
     function setupNewToken(string memory _uri, uint256 maxSupply) external returns (uint256 tokenId);
 
-    function setTokenMetadataRenderer(uint256 tokenId, IRenderer1155 renderer, bytes calldata setupData) external;
+    function setTokenMetadataRenderer(
+        uint256 tokenId,
+        IRenderer1155 renderer,
+        bytes calldata setupData
+    ) external;
 
     function contractURI() external view returns (string memory);
 
-    function isAdminOrRole(address user, uint256 tokenId, uint256 role) external view returns (bool);
+    function isAdminOrRole(
+        address user,
+        uint256 tokenId,
+        uint256 role
+    ) external view returns (bool);
+
+    function getTokenInfo(uint256 tokenId) external view returns (TokenData memory);
+
+    function callRenderer(uint256 tokenId, bytes memory data) external;
+
+    function callSale(
+        uint256 tokenId,
+        IMinter1155 salesConfig,
+        bytes memory data
+    ) external;
 }
