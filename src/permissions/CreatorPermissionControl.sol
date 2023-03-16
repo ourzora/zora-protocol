7aa566b70034fdea21d06c52d4913698b82b461f
Iain <iain@zora.co>
2023-03-16 16:06:45 -0400

    update permissions key logic

--- src/permissions/CreatorPermissionControl.sol
+++ src/permissions/CreatorPermissionControl.sol
@@ -5,39 +5,31 @@ import {CreatorPermissionStorageV1} from "./CreatorPermissionStorageV1.sol";
 import {ICreatorPermissionControl} from "../interfaces/ICreatorPermissionControl.sol";
 
 contract CreatorPermissionControl is CreatorPermissionStorageV1, ICreatorPermissionControl {
-    /// @notice Return the permission bits for a given user and token combo
-    function getPermissionKey(uint256 token, address user) internal pure returns (uint256) {
-        return uint256(keccak256(abi.encode(token, user)));
-    }
-
-    function _hasPermission(uint256 token, address user, uint256 permissionBits) internal view returns (bool) {
-        return permissions[getPermissionKey(token, user)] & permissionBits > 0;
+    function _hasPermission(uint256 tokenId, address user, uint256 permissionBits) internal view returns (bool) {
+        return permissions[tokenId][user] & permissionBits > 0;
     }
 
     /// @notice return the permission bits for a given user and token combo
     function getPermissions(uint256 token, address user) external view returns (uint256) {
-        return permissions[getPermissionKey(token, user)];
+        return permissions[token][user];
     }
 
     function _addPermission(uint256 tokenId, address user, uint256 permissionBits) internal {
-        uint256 permissionKey = getPermissionKey(tokenId, user);
-        uint256 tokenPermissions = permissions[permissionKey];
+        uint256 tokenPermissions = permissions[tokenId][user];
         tokenPermissions |= permissionBits;
-        permissions[permissionKey] = tokenPermissions;
+        permissions[tokenId][user] = tokenPermissions;
         emit UpdatedPermissions(tokenId, user, tokenPermissions);
     }
 
     function _clearPermissions(uint256 tokenId, address user) internal {
-        uint256 permissionKey = getPermissionKey(tokenId, user);
-        permissions[permissionKey] = 0;
+        permissions[tokenId][user] = 0;
         emit UpdatedPermissions(tokenId, user, 0);
     }
 
     function _removePermission(uint256 tokenId, address user, uint256 permissionBits) internal {
-        uint256 permissionKey = getPermissionKey(tokenId, user);
-        uint256 tokenPermissions = permissions[permissionKey];
+        uint256 tokenPermissions = permissions[tokenId][user];
         tokenPermissions &= ~permissionBits;
-        permissions[permissionKey] = tokenPermissions;
+        permissions[tokenId][user] = tokenPermissions;
         emit UpdatedPermissions(tokenId, user, tokenPermissions);
     }
 }
