9b70c9236c0dbeb8e5235b6ae5d82b934646f97e
Iain <iain@zora.co>
2023-03-24 15:20:22 -0400

    update scripts

---
+++ src/utils/ProxyShim.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+/// Used to deploy the factory before we know the impl address
+contract ProxyShim is UUPSUpgradeable {
+    address immutable canUpgrade;
+
+    constructor(address _canUpgrade) {
+        canUpgrade = _canUpgrade;
+    }
+
+    function _authorizeUpgrade(address) internal view override {
+        require(msg.sender == canUpgrade, "not authorized");
+    }
+}
