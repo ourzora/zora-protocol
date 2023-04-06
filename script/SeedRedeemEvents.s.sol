f54a57273de0963b21db2987b858de40c236a5c8
James Geary <36774175+jgeary@users.noreply.github.com>
2023-04-06 17:53:09 -0400

    suspected upgrade issue

--- script/SeedRedeemEvents.s.sol
+++ script/SeedRedeemEvents.s.sol
@@ -9,6 +9,7 @@ import {ERC1155PresetMinterPauser} from "@openzeppelin/contracts/token/ERC1155/p
 
 import {ZoraCreator1155FactoryImpl} from "../src/factory/ZoraCreator1155FactoryImpl.sol";
 import {Zora1155} from "../src/proxies/Zora1155.sol";
+import {ProxyShim} from "../src/utils/ProxyShim.sol";
 import {Zora1155Factory} from "../src/proxies/Zora1155Factory.sol";
 import {ZoraCreator1155Impl} from "../src/nft/ZoraCreator1155Impl.sol";
 import {ICreatorRoyaltiesControl} from "../src/interfaces/ICreatorRoyaltiesControl.sol";
@@ -27,22 +28,43 @@ contract SeedRedeemEvents is Script, Test {
 
         vm.startBroadcast(deployerPrivateKey);
 
+        ZoraCreatorFixedPriceSaleStrategy fixedPricedMinter = new ZoraCreatorFixedPriceSaleStrategy();
+        ZoraCreatorMerkleMinterStrategy merkleMinter = new ZoraCreatorMerkleMinterStrategy();
+        ZoraCreatorRedeemMinterFactory redeemMinterFactory = new ZoraCreatorRedeemMinterFactory();
+
+        address factoryShimAddress = address(new ProxyShim(deployer));
+        Zora1155Factory factoryProxy = new Zora1155Factory(factoryShimAddress, "");
+
+        ZoraCreator1155Impl creatorImpl = new ZoraCreator1155Impl(0, address(0), address(factoryProxy));
+
+        ZoraCreator1155FactoryImpl factoryImpl = new ZoraCreator1155FactoryImpl({
+            _implementation: creatorImpl,
+            _merkleMinter: merkleMinter,
+            _redeemMinterFactory: redeemMinterFactory,
+            _fixedPriceMinter: fixedPricedMinter
+        });
+
+        ZoraCreator1155FactoryImpl zora1155Factory = ZoraCreator1155FactoryImpl(address(factoryProxy));
+
+        // Upgrade to "real" factory address
+        ZoraCreator1155FactoryImpl(address(factoryProxy)).upgradeTo(address(factoryImpl));
+        ZoraCreator1155FactoryImpl(address(factoryProxy)).initialize(deployer);
+
         // existing goerli factories
         ICreatorRoyaltiesControl.RoyaltyConfiguration memory defaultRoyalties;
-        ZoraCreator1155FactoryImpl zora1155Factory = ZoraCreator1155FactoryImpl(0x8732b4bCa198509bB9c40f9a24638Be1eaB7D30c);
-        ZoraCreatorRedeemMinterFactory redeemMinterFactory = ZoraCreatorRedeemMinterFactory(0xE29a6D771df7d614ECBe5EBaE4107cf2440AeA9c);
+        // ZoraCreatorRedeemMinterFactory redeemMinterFactory = ZoraCreatorRedeemMinterFactory(0xE29a6D771df7d614ECBe5EBaE4107cf2440AeA9c);
 
         // test redeem tokens
         // ERC20PresetMinterPauser burnTokenERC20 = ERC20PresetMinterPauser(0xF9E8FF5535AC2296B2155844AdfB8d4Cf5306A73);
-        ERC20PresetMinterPauser burnTokenERC20 = new ERC20PresetMinterPauser("Random Token", "RAND");
-        burnTokenERC20.mint(address(deployer), 1000);
+        // ERC20PresetMinterPauser burnTokenERC20 = new ERC20PresetMinterPauser("Random Token", "RAND");
+        // burnTokenERC20.mint(address(deployer), 1000);
         // ERC1155PresetMinterPauser burnTokenERC1155 = ERC1155PresetMinterPauser(0x7820Accd4a7B9e98a1DC7d8a9965ED0B2C4B39C4);
         ERC1155PresetMinterPauser burnTokenERC1155 = new ERC1155PresetMinterPauser("https://zora.co/testing/token.json");
         burnTokenERC1155.mint(address(deployer), 1, 1000, "");
         // ERC721PresetMinterPauserAutoId burnTokenERC721 = ERC721PresetMinterPauserAutoId(0x5D3d4593a3b80d737e8782100E1f927fAB414DC2);
-        ERC721PresetMinterPauserAutoId burnTokenERC721 = new ERC721PresetMinterPauserAutoId("Test token", "TEST", "https://zora.co/testing/token.json");
-        burnTokenERC721.mint(address(deployer));
-        burnTokenERC721.mint(address(deployer));
+        // ERC721PresetMinterPauserAutoId burnTokenERC721 = new ERC721PresetMinterPauserAutoId("Test token", "TEST", "https://zora.co/testing/token.json");
+        // burnTokenERC721.mint(address(deployer));
+        // burnTokenERC721.mint(address(deployer));
 
         // new 1155
         bytes[] memory emptyBytes;
@@ -58,44 +80,44 @@ contract SeedRedeemEvents is Script, Test {
         test1155.addPermission(0, redeemMinterAddress, test1155.PERMISSION_BIT_MINTER());
         test1155.addPermission(1, redeemMinterAddress, test1155.PERMISSION_BIT_MINTER());
         ZoraCreatorRedeemMinterStrategy redeemMinter = ZoraCreatorRedeemMinterStrategy(redeemMinterAddress);
-        burnTokenERC20.approve(address(redeemMinter), 500);
+        // burnTokenERC20.approve(address(redeemMinter), 500);
         burnTokenERC1155.setApprovalForAll(address(redeemMinter), true);
-        burnTokenERC721.setApprovalForAll(address(redeemMinter), true);
+        // burnTokenERC721.setApprovalForAll(address(redeemMinter), true);
         // create multi-token redeem instructions
         ZoraCreatorRedeemMinterStrategy.MintToken memory mintToken = ZoraCreatorRedeemMinterStrategy.MintToken({
             tokenContract: address(test1155),
             tokenId: newTokenId,
-            amount: 10,
+            amount: 1,
             tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC1155
         });
-        ZoraCreatorRedeemMinterStrategy.RedeemInstruction[] memory instructions = new ZoraCreatorRedeemMinterStrategy.RedeemInstruction[](3);
+        ZoraCreatorRedeemMinterStrategy.RedeemInstruction[] memory instructions = new ZoraCreatorRedeemMinterStrategy.RedeemInstruction[](1);
+        // instructions[0] = ZoraCreatorRedeemMinterStrategy.RedeemInstruction({
+        //     tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC20,
+        //     amount: 500,
+        //     tokenIdStart: 0,
+        //     tokenIdEnd: 0,
+        //     tokenContract: address(burnTokenERC20),
+        //     transferRecipient: address(0),
+        //     burnFunction: bytes4(keccak256(bytes("burnFrom(address,uint256)")))
+        // });
         instructions[0] = ZoraCreatorRedeemMinterStrategy.RedeemInstruction({
-            tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC20,
-            amount: 500,
-            tokenIdStart: 0,
-            tokenIdEnd: 0,
-            tokenContract: address(burnTokenERC20),
-            transferRecipient: address(0),
-            burnFunction: bytes4(keccak256(bytes("burnFrom(address,uint256)")))
-        });
-        instructions[1] = ZoraCreatorRedeemMinterStrategy.RedeemInstruction({
             tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC1155,
             amount: 500,
             tokenIdStart: 1,
             tokenIdEnd: 1,
             tokenContract: address(burnTokenERC1155),
-            transferRecipient: address(0),
-            burnFunction: bytes4(keccak256(bytes("burnBatch(address,uint256[],uint256[])")))
+            transferRecipient: address(1),
+            burnFunction: bytes4(0)
         });
-        instructions[2] = ZoraCreatorRedeemMinterStrategy.RedeemInstruction({
-            tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC721,
-            amount: 2,
-            tokenIdStart: 0,
-            tokenIdEnd: 1,
-            tokenContract: address(burnTokenERC721),
-            transferRecipient: address(0),
-            burnFunction: bytes4(keccak256(bytes("burn(uint256)")))
-        });
+        // instructions[2] = ZoraCreatorRedeemMinterStrategy.RedeemInstruction({
+        //     tokenType: ZoraCreatorRedeemMinterStrategy.TokenType.ERC721,
+        //     amount: 2,
+        //     tokenIdStart: 0,
+        //     tokenIdEnd: 1,
+        //     tokenContract: address(burnTokenERC721),
+        //     transferRecipient: address(0),
+        //     burnFunction: bytes4(keccak256(bytes("burn(uint256)")))
+        // });
         ZoraCreatorRedeemMinterStrategy.RedeemInstructions memory redeemInstructions = ZoraCreatorRedeemMinterStrategy.RedeemInstructions({
             mintToken: mintToken,
             instructions: instructions,
@@ -108,21 +130,23 @@ contract SeedRedeemEvents is Script, Test {
         test1155.callSale(newTokenId, redeemMinter, abi.encodeWithSelector(ZoraCreatorRedeemMinterStrategy.setRedeem.selector, redeemInstructions));
 
         // purchase via burn to redeem
-        uint256[][] memory tokenIds = new uint256[][](3);
-        uint256[][] memory amounts = new uint256[][](3);
+        uint256[][] memory tokenIds = new uint256[][](1);
+        uint256[][] memory amounts = new uint256[][](1);
         // erc20 - 500 amt
+        // amounts[0] = new uint256[](1);
+        // amounts[0][0] = 500;
+        // erc1155 - tokenid 1, 500 amt
+        tokenIds[0] = new uint256[](1);
+        tokenIds[0][0] = 1;
         amounts[0] = new uint256[](1);
         amounts[0][0] = 500;
-        // erc1155 - tokenid 1, 500 amt
-        tokenIds[1] = new uint256[](1);
-        tokenIds[1][0] = 1;
-        amounts[1] = new uint256[](1);
-        amounts[1][0] = 500;
         // erc721 - tokenid 0, 1
-        tokenIds[2] = new uint256[](2);
-        tokenIds[2][0] = 0;
-        tokenIds[2][1] = 1;
+        // tokenIds[2] = new uint256[](2);
+        // tokenIds[2][0] = 0;
+        // tokenIds[2][1] = 1;
 
-        test1155.mint(redeemMinter, newTokenId, 10, abi.encode(redeemInstructions, tokenIds, amounts));
+        test1155.mint(redeemMinter, newTokenId, 1, abi.encode(redeemInstructions, tokenIds, amounts));
+
+        revert("done");
     }
 }
