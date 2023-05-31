11d131fb9a11d1150387e9a10d4c8b2e83cf996d
kulkarohan <kulkarohan@gmail.com>
2023-05-31 12:51:45 -0400

    refactor: add reward types

--- src/interfaces/IRewardsManager.sol
+++ src/interfaces/IRewardsManager.sol
@@ -2,24 +2,61 @@
 pragma solidity 0.8.17;
 
 interface IRewardsManager {
-    event RewardsAdded(address indexed recipient1, uint256 amount1, address indexed recipient2, uint256 amount2, address indexed recipient3, uint256 amount3);
+    event RewardsAdded(bytes4 indexed rewardType, address recipient, uint256 amount);
+    event RewardsAdded(bytes4 indexed rewardType, address recipient1, uint256 amount1, address recipient2, uint256 amount2);
     event RewardsAdded(
-        address indexed recipient1,
+        bytes4 indexed rewardType,
+        address recipient1,
         uint256 amount1,
-        address indexed recipient2,
+        address recipient2,
         uint256 amount2,
-        address indexed recipient3,
+        address recipient3,
+        uint256 amount3
+    );
+    event RewardsAdded(
+        bytes4 indexed rewardType,
+        address recipient1,
+        uint256 amount1,
+        address recipient2,
+        uint256 amount2,
+        address recipient3,
         uint256 amount3,
         address recipient4,
         uint256 amount4
+    );
+    event RewardsAdded(
+        bytes4 indexed rewardType,
+        address recipient1,
+        uint256 amount1,
+        address recipient2,
+        uint256 amount2,
+        address recipient3,
+        uint256 amount3,
+        address recipient4,
+        uint256 amount4,
+        address recipient5,
+        uint256 amount5
     );
 
     error INVALID_AMOUNT();
     error FAILED_WITHDRAW();
 
-    function addReward(address recipient1, uint256 amount1, address recipient2, uint256 amount2, address recipient3, uint256 amount3) external payable;
+    function addReward(bytes4 rewardType, address recipient, uint256 amount) external payable;
+
+    function addReward(bytes4 rewardType, address recipient1, uint256 amount1, address recipient2, uint256 amount2) external payable;
 
     function addReward(
+        bytes4 rewardType,
+        address recipient1,
+        uint256 amount1,
+        address recipient2,
+        uint256 amount2,
+        address recipient3,
+        uint256 amount3
+    ) external payable;
+
+    function addReward(
+        bytes4 rewardType,
         address recipient1,
         uint256 amount1,
         address recipient2,
@@ -30,5 +67,21 @@ interface IRewardsManager {
         uint256 amount4
     ) external payable;
 
+    function addReward(
+        bytes4 rewardType,
+        address recipient1,
+        uint256 amount1,
+        address recipient2,
+        uint256 amount2,
+        address recipient3,
+        uint256 amount3,
+        address recipient4,
+        uint256 amount4,
+        address recipient5,
+        uint256 amount5
+    ) external payable;
+
     function withdrawReward() external;
+
+    function withdrawReward(address recipient) external;
 }
--- src/rewards/RewardsManager.sol
+++ src/rewards/RewardsManager.sol
@@ -1,59 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IRewardsManager {
-    event RewardsAdded(address indexed recipient1, uint256 amount1, address indexed recipient2, uint256 amount2, address indexed recipient3, uint256 amount3);
-    event RewardsAdded(
-        address indexed recipient1,
-        uint256 amount1,
-        address indexed recipient2,
-        uint256 amount2,
-        address indexed recipient3,
-        uint256 amount3,
-        address recipient4,
-        uint256 amount4
-    );
+import {IRewardsManager} from "../interfaces/IRewardsManager.sol";
 
-    error INVALID_AMOUNT();
-    error FAILED_WITHDRAW();
+contract RewardsManager is IRewardsManager {
+    bytes4 public constant ZORA_FREE_MINT_REWARD_TYPE = bytes4(keccak256("ZORA_FREE_MINT_REWARDS"));
+    bytes4 public constant ZORA_PAID_MINT_REWARD_TYPE = bytes4(keccak256("ZORA_PAID_MINT_REWARDS"));
 
-    function addReward(address recipient1, uint256 amount1, address recipient2, uint256 amount2, address recipient3, uint256 amount3) external payable;
+    mapping(address => uint256) public balanceOf;
+
+    function addReward(bytes4 rewardType, address recipient, uint256 amount) external payable {
+        if (msg.value != amount) {
+            revert INVALID_AMOUNT();
+        }
+
+        balanceOf[recipient] += amount;
+
+        emit RewardsAdded(rewardType, recipient, amount);
+    }
+
+    function addReward(bytes4 rewardType, address recipient1, uint256 amount1, address recipient2, uint256 amount2) external payable {
+        if (msg.value != (amount1 + amount2)) {
+            revert INVALID_AMOUNT();
+        }
+
+        balanceOf[recipient1] += amount1;
+        balanceOf[recipient2] += amount2;
+
+        emit RewardsAdded(rewardType, recipient1, amount1, recipient2, amount2);
+    }
 
     function addReward(
+        bytes4 rewardType,
         address recipient1,
         uint256 amount1,
         address recipient2,
         uint256 amount2,
         address recipient3,
-        uint256 amount3,
-        address recipient4,
-        uint256 amount4
-    ) external payable;
-
-    function withdrawReward() external;
-}
-
-/** TODO
-- variants of addReward params and events
-- withdraw w/ recipient
-- withdraw w/ sig?
- */
-contract RewardsManager is IRewardsManager {
-    mapping(address => uint256) public ethBalance;
-
-    function addReward(address recipient1, uint256 amount1, address recipient2, uint256 amount2, address recipient3, uint256 amount3) external payable {
+        uint256 amount3
+    ) external payable {
         if (msg.value != (amount1 + amount2 + amount3)) {
             revert INVALID_AMOUNT();
         }
 
-        ethBalance[recipient1] += amount1;
-        ethBalance[recipient2] += amount2;
-        ethBalance[recipient3] += amount3;
+        balanceOf[recipient1] += amount1;
+        balanceOf[recipient2] += amount2;
+        balanceOf[recipient3] += amount3;
 
-        emit RewardsAdded(recipient1, amount1, recipient2, amount2, recipient3, amount3);
+        emit RewardsAdded(rewardType, recipient1, amount1, recipient2, amount2, recipient3, amount3);
     }
 
     function addReward(
+        bytes4 rewardType,
         address recipient1,
         uint256 amount1,
         address recipient2,
@@ -67,22 +65,62 @@ contract RewardsManager is IRewardsManager {
             revert INVALID_AMOUNT();
         }
 
-        ethBalance[recipient1] += amount1;
-        ethBalance[recipient2] += amount2;
-        ethBalance[recipient3] += amount3;
-        ethBalance[recipient4] += amount4;
+        balanceOf[recipient1] += amount1;
+        balanceOf[recipient2] += amount2;
+        balanceOf[recipient3] += amount3;
+        balanceOf[recipient4] += amount4;
 
-        emit RewardsAdded(recipient1, amount1, recipient2, amount2, recipient3, amount3, recipient4, amount4);
+        emit RewardsAdded(rewardType, recipient1, amount1, recipient2, amount2, recipient3, amount3, recipient4, amount4);
+    }
+
+    function addReward(
+        bytes4 rewardType,
+        address recipient1,
+        uint256 amount1,
+        address recipient2,
+        uint256 amount2,
+        address recipient3,
+        uint256 amount3,
+        address recipient4,
+        uint256 amount4,
+        address recipient5,
+        uint256 amount5
+    ) external payable {
+        if (msg.value != (amount1 + amount2 + amount3 + amount4 + amount5)) {
+            revert INVALID_AMOUNT();
+        }
+
+        balanceOf[recipient1] += amount1;
+        balanceOf[recipient2] += amount2;
+        balanceOf[recipient3] += amount3;
+        balanceOf[recipient4] += amount4;
+        balanceOf[recipient5] += amount5;
+
+        emit RewardsAdded(rewardType, recipient1, amount1, recipient2, amount2, recipient3, amount3, recipient4, amount4, recipient5, amount5);
     }
 
     function withdrawReward() external {
         address user = msg.sender;
 
-        uint256 amount = ethBalance[user];
+        uint256 amount = balanceOf[user];
 
-        delete ethBalance[user];
+        delete balanceOf[user];
 
         (bool success, ) = user.call{value: amount, gas: 110_000}("");
+
+        if (!success) {
+            revert FAILED_WITHDRAW();
+        }
+    }
+
+    function withdrawReward(address recipient) external {
+        address user = msg.sender;
+
+        uint256 amount = balanceOf[user];
+
+        delete balanceOf[user];
+
+        (bool success, ) = recipient.call{value: amount, gas: 110_000}("");
 
         if (!success) {
             revert FAILED_WITHDRAW();
