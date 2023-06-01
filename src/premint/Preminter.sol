dd80346f0460cb981047e3817b62287cccc60a37
Dan Oved <stangogh@gmail.com>
2023-06-01 16:04:34 -0700

    Full implementation written, now time for tests

--- src/premint/Preminter.sol
+++ src/premint/Preminter.sol
@@ -11,11 +11,6 @@ import {SharedBaseConstants} from "../shared/SharedBaseConstants.sol";
 import {ZoraCreatorFixedPriceSaleStrategy} from "../minters/fixed-price/ZoraCreatorFixedPriceSaleStrategy.sol";
 
 contract Preminter is EIP712UpgradeableWithChainId {
-    bytes32 constant PREMINT_TYPEHASH =
-        keccak256(
-            "delegateCreate(address contractAdmin,bytes contractURI,bytes contractName,ICreatorRoyaltiesControl.RoyaltyConfiguration defaultRoyaltyConfiguration,bytes tokenURI,uint256 tokenMaxSupply,PremintFixedPriceSalesConfig tokenSalesConfig,uint256 quantityToMint)"
-        );
-
     IZoraCreator1155Factory factory;
     ZoraCreatorFixedPriceSaleStrategy fixedPriceSaleStrategy;
 
@@ -29,7 +24,7 @@ contract Preminter is EIP712UpgradeableWithChainId {
     uint256 public constant PERMISSION_BIT_MINTER = 2 ** 2;
 
     mapping(uint256 => address) contractAddresses;
-    mapping(address => mapping(uint64 => bool)) nonces;
+    mapping(uint256 => bool) signatureUsed;
 
     function initialize(IZoraCreator1155Factory _factory, ZoraCreatorFixedPriceSaleStrategy _fixedPriceSaleStrategy) public initializer {
         __EIP712_init("Preminter", "0.0.1");
@@ -46,22 +41,27 @@ contract Preminter is EIP712UpgradeableWithChainId {
         uint64 duration;
     }
 
+    struct ContractCreationConfig {
+        address contractAdmin;
+        string contractURI;
+        string contractName;
+        ICreatorRoyaltiesControl.RoyaltyConfiguration defaultRoyaltyConfiguration;
+    }
+
+    struct TokenCreationConfig {
+        string tokenURI;
+        uint256 tokenMaxSupply;
+        PremintFixedPriceSalesConfig tokenSalesConfig;
+    }
+
     // same signature should work whether or not there is an existing contract
     // so it is unaware of order, it just takes the token uri and creates the next token with it
     // this could include creating the contract.
-
     // do we need a deadline? open q
 
-    // forward payable
-
     function premint(
-        address payable contractAdmin,
-        string calldata contractURI,
-        string calldata contractName,
-        ICreatorRoyaltiesControl.RoyaltyConfiguration calldata defaultRoyaltyConfiguration,
-        string calldata tokenURI,
-        uint256 tokenMaxSupply,
-        PremintFixedPriceSalesConfig calldata tokenSalesConfig,
+        ContractCreationConfig calldata contractConfig,
+        TokenCreationConfig calldata tokenConfig,
         uint256 quantityToMint,
         bytes calldata signature
     ) public payable returns (uint256 newTokenId) {
@@ -73,56 +73,48 @@ contract Preminter is EIP712UpgradeableWithChainId {
         // 5. Mint x tokens, as configured, to the executor of this transaction.
 
         // first validate the signature - the creator must match the signer of the message
-        bytes32 digest = premintHashData(
-            contractAdmin,
-            contractURI,
-            contractName,
-            defaultRoyaltyConfiguration,
-            tokenURI,
-            tokenMaxSupply,
-            tokenSalesConfig,
+        (bytes32 digest, uint256 contractHashId, uint256 tokenHash) = premintHashData(
+            contractConfig,
+            tokenConfig,
             quantityToMint,
+            // here we pass the current chain id, ensuring that the message
+            // only works for the current chain id
             block.chainid
         );
 
+        // token hash includes the contract hash, so we can check uniqueness of contract + token pair
+        if (signatureUsed[tokenHash]) {
+            revert("Signature already used");
+        }
+        signatureUsed[tokenHash] = true;
+
         address signatory = ECDSAUpgradeable.recover(digest, signature);
-        if (signatory != contractAdmin) revert("Invalid signature");
+        if (signatory != contractConfig.contractAdmin) {
+            revert("Invalid signature");
+        }
 
-        (IZoraCreator1155 tokenContract, uint256 contractHash) = _getOrCreateContract(contractAdmin, contractURI, contractName, defaultRoyaltyConfiguration);
+        IZoraCreator1155 tokenContract = _getOrCreateContract(contractConfig, contractHashId);
 
-        _setupNewTokenAndSale(tokenContract, contractAdmin, tokenURI, tokenMaxSupply, tokenSalesConfig);
+        _setupNewTokenAndSale(tokenContract, contractConfig.contractAdmin, tokenConfig);
 
         // we mint the initial x tokens for this new token id to the executor.
         address tokenRecipient = msg.sender;
         tokenContract.mint{value: msg.value}(fixedPriceSaleStrategy, newTokenId, quantityToMint, abi.encode(tokenRecipient, ""));
     }
 
-    function _getOrCreateContract(
-        address payable contractAdmin,
-        string calldata contractURI,
-        string calldata contractName,
-        ICreatorRoyaltiesControl.RoyaltyConfiguration calldata defaultRoyaltyConfiguration
-    ) private returns (IZoraCreator1155 tokenContract, uint256 contractHash) {
-        // get unique hash based on contract params
-        contractHash = _contractDataHash(contractAdmin, contractURI, contractName, defaultRoyaltyConfiguration);
-
+    function _getOrCreateContract(ContractCreationConfig calldata contractConfig, uint256 contractHash) private returns (IZoraCreator1155 tokenContract) {
         address contractAddress = contractAddresses[contractHash];
         // if address already exists for hash, return it
         if (contractAddress != address(0)) {
             tokenContract = IZoraCreator1155(contractAddress);
         } else {
             // otherwise, create the contract and update the created contracts
-            tokenContract = _createContract(contractAdmin, contractURI, contractName, defaultRoyaltyConfiguration);
+            tokenContract = _createContract(contractConfig);
             contractAddresses[contractHash] = address(tokenContract);
         }
     }
 
-    function _createContract(
-        address payable contractAdmin,
-        string calldata contractURI,
-        string calldata contractName,
-        ICreatorRoyaltiesControl.RoyaltyConfiguration calldata defaultRoyaltyConfiguration
-    ) private returns (IZoraCreator1155 tokenContract) {
+    function _createContract(ContractCreationConfig calldata contractConfig) private returns (IZoraCreator1155 tokenContract) {
         // we need to build the setup actions, that must:
         // grant this contract ability to mint tokens - when a token is minted, this contract is
         // granted admin rights on that token
@@ -130,32 +122,23 @@ contract Preminter is EIP712UpgradeableWithChainId {
         setupActions[0] = abi.encodeWithSelector(IZoraCreator1155.addPermission.selector, CONTRACT_BASE_ID, address(this), PERMISSION_BIT_MINTER);
 
         // create the contract via the factory.
-        address newContractAddresss = factory.createContract(contractURI, contractName, defaultRoyaltyConfiguration, contractAdmin, setupActions);
+        address newContractAddresss = factory.createContract(
+            contractConfig.contractURI,
+            contractConfig.contractName,
+            contractConfig.defaultRoyaltyConfiguration,
+            payable(contractConfig.contractAdmin),
+            setupActions
+        );
         tokenContract = IZoraCreator1155(newContractAddresss);
-    }
-
-    function _contractDataHash(
-        address contractAdmin,
-        string calldata contractURI,
-        string calldata contractName,
-        ICreatorRoyaltiesControl.RoyaltyConfiguration calldata defaultRoyaltyConfiguration
-    ) private pure returns (uint256) {
-        return uint256(keccak256(abi.encode(contractAdmin, _bytesEncode(contractURI), _bytesEncode(contractName), defaultRoyaltyConfiguration)));
-    }
-
-    function _bytesEncode(string calldata value) private pure returns (bytes32) {
-        return keccak256(abi.encode(value));
     }
 
     function _setupNewTokenAndSale(
         IZoraCreator1155 tokenContract,
         address contractAdmin,
-        string calldata tokenURI,
-        uint256 tokenMaxSupply,
-        PremintFixedPriceSalesConfig calldata tokenSalesConfig
+        TokenCreationConfig calldata tokenConfig
     ) private returns (uint256 newTokenId) {
         // we then mint a new token, and get its token id
-        newTokenId = tokenContract.setupNewToken(tokenURI, tokenMaxSupply);
+        newTokenId = tokenContract.setupNewToken(tokenConfig.tokenURI, tokenConfig.tokenMaxSupply);
         // we then set up the sales strategy
         // first we grant the fixed price sale strategy minting capabilities
         tokenContract.addPermission(newTokenId, address(fixedPriceSaleStrategy), PERMISSION_BIT_MINTER);
@@ -163,7 +146,11 @@ contract Preminter is EIP712UpgradeableWithChainId {
         tokenContract.callSale(
             newTokenId,
             fixedPriceSaleStrategy,
-            abi.encodeWithSelector(ZoraCreatorFixedPriceSaleStrategy.setSale.selector, newTokenId, _buildNewSalesConfig(contractAdmin, tokenSalesConfig))
+            abi.encodeWithSelector(
+                ZoraCreatorFixedPriceSaleStrategy.setSale.selector,
+                newTokenId,
+                _buildNewSalesConfig(contractAdmin, tokenConfig.tokenSalesConfig)
+            )
         );
         // this contract is the admin of that token - lets make the contract
         // we make the creator the admin of that token, we remove this contracts admin rights on that token.
@@ -171,32 +158,47 @@ contract Preminter is EIP712UpgradeableWithChainId {
         tokenContract.removePermission(newTokenId, address(this), PERMISSION_BIT_ADMIN);
     }
 
+    bytes32 constant PREMINT_TYPEHASH = keccak256("delegateCreate(uint256 contractHash,uint256 tokenHash,uint256 quantityToMint)");
+
     function premintHashData(
-        address contractAdmin,
-        string calldata contractURI,
-        string calldata contractName,
-        ICreatorRoyaltiesControl.RoyaltyConfiguration memory defaultRoyaltyConfiguration,
-        string calldata tokenURI,
-        uint256 tokenMaxSupply,
-        PremintFixedPriceSalesConfig calldata fixedPriceSalesConfig,
+        ContractCreationConfig calldata contractConfig,
+        TokenCreationConfig calldata tokenConfig,
         uint256 quantityToMint,
         uint256 chainId
-    ) public view returns (bytes32) {
-        bytes32 structHash = keccak256(
-            abi.encode(
-                PREMINT_TYPEHASH,
-                contractAdmin,
-                bytes(contractURI),
-                bytes(contractName),
-                defaultRoyaltyConfiguration,
-                bytes(tokenURI),
-                tokenMaxSupply,
-                fixedPriceSalesConfig,
-                quantityToMint
-            )
+    ) public view returns (bytes32 structHash, uint256 contractHash, uint256 tokenHash) {
+        contractHash = _contractDataHash(contractConfig);
+        tokenHash = _tokenDataHash(contractHash, tokenConfig);
+        bytes32 encoded = keccak256(
+            abi.encode(PREMINT_TYPEHASH, contractHash, bytes(tokenConfig.tokenURI), tokenConfig.tokenMaxSupply, tokenConfig.tokenSalesConfig, quantityToMint)
         );
 
-        return _hashTypedDataV4(structHash, chainId);
+        // build the struct hash to be signed
+        // here we pass the chain id, allowing the message to be signed for another chain
+        structHash = _hashTypedDataV4(encoded, chainId);
+    }
+
+    /// returns a unique hash for the contract data, useful to uniquely identify a contract based on creation params
+    function _contractDataHash(ContractCreationConfig calldata contractConfig) private pure returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encode(
+                        contractConfig.contractAdmin,
+                        _bytesEncode(contractConfig.contractURI),
+                        _bytesEncode(contractConfig.contractName),
+                        contractConfig.defaultRoyaltyConfiguration
+                    )
+                )
+            );
+    }
+
+    /// Return a unique hash for a token creation config, unique within a contract. Used to check if a token has already been created with these params
+    function _tokenDataHash(uint256 contractHash, TokenCreationConfig calldata tokenConfig) private pure returns (uint256) {
+        return uint256(keccak256(abi.encode(contractHash, bytes(tokenConfig.tokenURI), tokenConfig.tokenMaxSupply, tokenConfig.tokenSalesConfig)));
+    }
+
+    function _bytesEncode(string calldata value) private pure returns (bytes32) {
+        return keccak256(abi.encode(value));
     }
 
     function _buildNewSalesConfig(
