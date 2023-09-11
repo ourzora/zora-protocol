63906414e2dbc512cd6a04947fbfb81628d5b490
Dan Oved <stangogh@gmail.com>
2023-09-11 12:49:20 -0700

    Contract size optimization - remove unneeded permission fetcher (#139)

--- src/interfaces/ICreatorPermissionControl.sol
+++ src/interfaces/ICreatorPermissionControl.sol
@@ -5,8 +5,4 @@ pragma solidity 0.8.17;
 interface ICreatorPermissionControl {
     /// @notice Emitted when permissions are updated
     event UpdatedPermissions(uint256 indexed tokenId, address indexed user, uint256 indexed permissions);
-
-    /// @notice Public interface to get permissions given a token id and a user address
-    /// @return Returns raw permission bits
-    function getPermissions(uint256 tokenId, address user) external view returns (uint256);
 }
--- test/mock/MockPermissionControl.sol
+++ test/mock/MockPermissionControl.sol
@@ -4,14 +4,6 @@ pragma solidity 0.8.17;
 import {CreatorPermissionControl} from "../../src/permissions/CreatorPermissionControl.sol";
 
 contract MockPermissionControl is CreatorPermissionControl {
-    function hasPermissions(uint256 tokenId, address user, uint256 permissionBits) external view returns (bool) {
-        return _hasPermissions(tokenId, user, permissionBits);
-    }
-
-    function hasAnyPermission(uint256 tokenId, address user, uint256 permissionBits) external view returns (bool) {
-        return _hasAnyPermission(tokenId, user, permissionBits);
-    }
-
     function addPermission(uint256 tokenId, address user, uint256 permissionBits) external {
         _addPermission(tokenId, user, permissionBits);
     }
