ef67a7d57bcd5c979406665328be0591dd39e6c1
Dan Oved <stangogh@gmail.com>
2023-09-21 13:26:19 -0700

    Contract size optimization - remove unused royalty fetcher to reduce contract size (#140)

--- test/royalties/CreatorRoyaltiesControl.t.sol
+++ test/royalties/CreatorRoyaltiesControl.t.sol
@@ -32,6 +32,19 @@ contract CreatorRoyaltiesControlTest is Test {
         response = new bytes[](0);
     }
 
+    /// @notice Returns the supply royalty information for a given token.
+    /// @param tokenId The token ID to get the royalty information for.
+    /// @param mintAmount The amount of tokens being minted.
+    /// @param totalSupply The total supply of the token,
+    function supplyRoyaltyInfo(uint256 tokenId, uint256 totalSupply, uint256 mintAmount) public view returns (address receiver, uint256 royaltyAmount) {
+        ICreatorRoyaltiesControl.RoyaltyConfiguration memory config = target.getRoyalties(tokenId);
+        if (config.royaltyMintSchedule == 0) {
+            return (config.royaltyRecipient, 0);
+        }
+        uint256 totalRoyaltyMints = (mintAmount + (totalSupply % config.royaltyMintSchedule)) / (config.royaltyMintSchedule - 1);
+        return (config.royaltyRecipient, totalRoyaltyMints);
+    }
+
     function test_GetsRoyaltiesInfoGlobalDefault() external {
         address royaltyPayout = address(0x999);
 
@@ -44,7 +57,7 @@ contract CreatorRoyaltiesControlTest is Test {
         uint256 tokenId = target.setupNewToken("test", 100);
 
         (address royaltyRecipient, uint256 amount) = target.royaltyInfo(tokenId, 1 ether);
-        (, uint256 supplyAmount) = target.supplyRoyaltyInfo(tokenId, 0, 100);
+        (, uint256 supplyAmount) = supplyRoyaltyInfo(tokenId, 0, 100);
         assertEq(amount, 0.001 ether);
         assertEq(royaltyRecipient, royaltyPayout);
         assertEq(supplyAmount, 11);
@@ -66,13 +79,13 @@ contract CreatorRoyaltiesControlTest is Test {
         vm.stopPrank();
 
         (address royaltyRecipient, uint256 amount) = target.royaltyInfo(tokenIdFirst, 1 ether);
-        (, uint256 supplyAmount) = target.supplyRoyaltyInfo(tokenIdFirst, 0, 100);
+        (, uint256 supplyAmount) = supplyRoyaltyInfo(tokenIdFirst, 0, 100);
         assertEq(amount, 0.001 ether);
         assertEq(supplyAmount, 1);
         assertEq(royaltyRecipient, royaltyPayout);
 
         (address royaltyRecipientSecond, uint256 amountSecond) = target.royaltyInfo(tokenIdSecond, 1 ether);
-        (, uint256 supplyAmountSecond) = target.supplyRoyaltyInfo(tokenIdSecond, 0, 100);
+        (, uint256 supplyAmountSecond) = supplyRoyaltyInfo(tokenIdSecond, 0, 100);
         assertEq(amountSecond, 0.01 ether);
         assertEq(supplyAmountSecond, 11);
         assertEq(royaltyRecipientSecond, address(0x992));
