bd93a068ef0ff10fe3bad74d4c1557c191d06c2a
Dan Oved <stangogh@gmail.com>
2023-09-26 17:16:40 -0400

    * Updated script to deploy preminter to now deploy the proxy (#189)
    * Extracted common deployment tasks to reusable helper functions
    * Added script to upgrade preminter

---
+++ script/UpgradePreminter.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+
+import {ZoraDeployerBase} from "./ZoraDeployerBase.sol";
+import {ChainConfig, Deployment} from "../src/deployment/DeploymentConfig.sol";
+import {UUPSUpgradeable} from "@zoralabs/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+
+contract UpgradePreminter is ZoraDeployerBase {
+    function run() public returns (string memory, bytes memory upgradeCalldata, address upgradeTarget) {
+        Deployment memory deployment = getDeployment();
+
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        address preminterImplementation = deployNewPreminterImplementation(deployment);
+
+        vm.stopBroadcast();
+
+        upgradeCalldata = abi.encodeWithSelector(UUPSUpgradeable.upgradeTo.selector, preminterImplementation);
+
+        upgradeTarget = deployment.preminter;
+
+        console2.log("Upgrade PremintExecutor target and implementatin:", upgradeTarget, preminterImplementation);
+        console2.log("To upgrade, use this calldata:");
+        console2.logBytes(upgradeCalldata);
+
+        return (getDeploymentJSON(deployment), upgradeCalldata, upgradeTarget);
+    }
+}
