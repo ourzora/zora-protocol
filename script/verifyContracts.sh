2cd1b99304edcb6a980c62362b2bb7f9d248c671
Dan Oved <stangogh@gmail.com>
2023-09-29 18:02:24 -0400

    contracts deployed to zora mainnet and testnet

--- script/verifyContracts.sh
+++ script/verifyContracts.sh
@@ -5,5 +5,8 @@
 #
 
 # verify factory proxy Zora1155Factory
-forge verify-contract 0x777777267FA8D1C26EF7A2dffb33A290464D1b4B --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf Zora1155Factory
-forge verify-contract 0x49e85797dc499b9132433a62f63ef80e76c07865 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf ZoraCreator1155Impl
+#forge verify-contract 0x777777267FA8D1C26EF7A2dffb33A290464D1b4B --chain 777777 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf Zora1155Factory
+#forge verify-contract 0x49e85797dc499b9132433a62f63ef80e76c07865 --chain 777777 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf ZoraCreator1155Impl
+#forge verify-contract 0x6E2AbBcd82935bFC68A1d5d2c96372b13b65eD9C --chain 777777 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf Zora1155PremintExecutorImpl
+#forge verify-contract 0x6E2AbBcd82935bFC68A1d5d2c96372b13b65eD9C --chain 777777 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf ZoraCreator1155PremintExecutorImpl
+forge verify-contract 0x8E024a6e8E9c1cbC5BfcC73e87409a6D98b3425d --chain 777777 --verifier-url $VERIFIER_URL --verifier blockscout --etherscan-api-key asdfasdf Zora1155
