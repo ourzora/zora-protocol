278704419f33a82269d2046ddedc03798eaf8fb4
Dan Oved <stangogh@gmail.com>
2023-10-30 17:30:44 -0600

    simplified wagmi config for 1155

--- packages/1155-contracts/wagmi.config.ts
+++ packages/1155-contracts/wagmi.config.ts
@@ -1,29 +1,6 @@
 import { defineConfig } from "@wagmi/cli";
 import { foundry } from "@wagmi/cli/plugins";
 
-type ContractNames =
-  | "ZoraCreator1155FactoryImpl"
-  | "ZoraCreator1155Impl"
-  | "ZoraCreatorFixedPriceSaleStrategy"
-  | "ZoraCreatorMerkleMinterStrategy"
-  | "ZoraCreatorRedeemMinterFactory"
-  | "ZoraCreatorRedeemMinterStrategy"
-  | "ZoraCreator1155PremintExecutorImpl"
-  | "DeterministicProxyDeployer"
-  | "IImmutableCreate2Factory";
-
-const contractFilesToInclude: ContractNames[] = [
-  "ZoraCreator1155FactoryImpl",
-  "ZoraCreator1155Impl",
-  "ZoraCreatorFixedPriceSaleStrategy",
-  "ZoraCreatorMerkleMinterStrategy",
-  "ZoraCreatorRedeemMinterFactory",
-  "ZoraCreatorRedeemMinterStrategy",
-  "ZoraCreator1155PremintExecutorImpl",
-  "DeterministicProxyDeployer",
-  "IImmutableCreate2Factory",
-];
-
 export default defineConfig({
   out: "package/wagmiGenerated.ts",
   plugins: [
@@ -31,9 +8,17 @@ export default defineConfig({
       forge: {
         build: false,
       },
-      include: contractFilesToInclude.map(
-        (contractName) => `${contractName}.json`
-      ),
+      include: [
+        "ZoraCreator1155FactoryImpl",
+        "ZoraCreator1155Impl",
+        "ZoraCreatorFixedPriceSaleStrategy",
+        "ZoraCreatorMerkleMinterStrategy",
+        "ZoraCreatorRedeemMinterFactory",
+        "ZoraCreatorRedeemMinterStrategy",
+        "ZoraCreator1155PremintExecutorImpl",
+        "DeterministicProxyDeployer",
+        "IImmutableCreate2Factory",
+      ].map((contractName) => `${contractName}.json`),
     }),
   ],
 });
