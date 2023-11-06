9222f506274402cd49c81b9071a68f3729b85def
Dan Oved <stangogh@gmail.com>
2023-11-06 15:32:37 -0800

    Fix premint v1 sigs

--- packages/premint-sdk/src/preminter.ts
+++ packages/premint-sdk/src/preminter.ts
@@ -20,7 +20,7 @@ export type ContractCreationConfig = PreminterV2HashDataTypes[0];
 export type PremintConfigV1 = PreminterV1HashDataTypes[1];
 export type PremintConfigV2 = PreminterV2HashDataTypes[1];
 
-export type TokenCreationConfigV1 = PremintConfigV2["tokenConfig"];
+export type TokenCreationConfigV1 = PremintConfigV1["tokenConfig"];
 export type TokenCreationConfigV2 = PremintConfigV2["tokenConfig"];
 
 const premintV2Types = {
@@ -47,6 +47,31 @@ const premintV2Types = {
     { name: "createReferral", type: "address" },
   ],
 };
+
+const premintV1Types = {
+    CreatorAttribution: [
+      { name: "tokenConfig", type: "TokenCreationConfig" },
+      // unique id scoped to the contract and token to create.
+      // ensure that a signature can be replaced, as long as the replacement
+      // has the same uid, and a newer version.
+      { name: "uid", type: "uint32" },
+      { name: "version", type: "uint32" },
+      // if this update should result in the signature being deleted.
+      { name: "deleted", type: "bool" },
+    ],
+    TokenCreationConfig: [
+      { name: "tokenURI", type: "string" },
+      { name: "maxSupply", type: "uint256" },
+      { name: "maxTokensPerAddress", type: "uint64" },
+      { name: "pricePerToken", type: "uint96" },
+      { name: "mintStart", type: "uint64" },
+      { name: "mintDuration", type: "uint64" },
+      { name: "royaltyMintSchedule", type: "uint32" },
+      { name: "royaltyBPS", type: "uint32" },
+      { name: "royaltyRecipient", type: "address" },
+      { name: "fixedPriceMinter", type: "address" },
+    ],
+  };
 
 // Convenience method to create the structured typed data
 // needed to sign for a premint contract and token
@@ -96,39 +121,16 @@ export const preminterTypedDataDefinitionV1 = ({
   chainId: number;
 }) => {
   const { tokenConfig, uid, version, deleted } = premintConfig;
-  const types = {
-    CreatorAttribution: [
-      { name: "tokenConfig", type: "TokenCreationConfig" },
-      // unique id scoped to the contract and token to create.
-      // ensure that a signature can be replaced, as long as the replacement
-      // has the same uid, and a newer version.
-      { name: "uid", type: "uint32" },
-      { name: "version", type: "uint32" },
-      // if this update should result in the signature being deleted.
-      { name: "deleted", type: "bool" },
-    ],
-    TokenCreationConfig: [
-      { name: "tokenURI", type: "string" },
-      { name: "maxSupply", type: "uint256" },
-      { name: "maxTokensPerAddress", type: "uint64" },
-      { name: "pricePerToken", type: "uint96" },
-      { name: "mintStart", type: "uint64" },
-      { name: "mintDuration", type: "uint64" },
-      { name: "royaltyMintSchedule", type: "uint32" },
-      { name: "royaltyBPS", type: "uint32" },
-      { name: "royaltyRecipient", type: "address" },
-      { name: "fixedPriceMinter", type: "address" },
-    ],
-  };
 
-  const result: TypedDataDefinition<typeof types, "CreatorAttribution"> = {
+
+  const result: TypedDataDefinition<typeof premintV1Types, "CreatorAttribution"> = {
     domain: {
       chainId,
       name: "Preminter",
       version: "1",
       verifyingContract: verifyingContract,
     },
-    types,
+    types: premintV1Types,
     message: {
       tokenConfig,
       uid,
