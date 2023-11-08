de3be04cde490fa6cd2e138f1244a094d8a16317
Rohan Kulkarni <kulkarohan@gmail.com>
2023-11-08 16:11:42 -0500

    feat: fixed price mint for allowed minters module

---
+++ packages/1155-contracts/src/minters/fixed-price/FixedPriceAllowedMintersStrategy.sol
@@ -0,0 +1,199 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {Enjoy} from "_imagine/mint/Enjoy.sol";
+import {IMinter1155} from "../../interfaces/IMinter1155.sol";
+import {ICreatorCommands} from "../../interfaces/ICreatorCommands.sol";
+import {SaleStrategy} from "../SaleStrategy.sol";
+import {SaleCommandHelper} from "../utils/SaleCommandHelper.sol";
+import {LimitedMintPerAddress} from "../utils/LimitedMintPerAddress.sol";
+import {IMinterErrors} from "../../interfaces/IMinterErrors.sol";
+
+/*
+
+
+             ░░░░░░░░░░░░░░              
+        ░░▒▒░░░░░░░░░░░░░░░░░░░░        
+      ░░▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░      
+    ░░▒▒▒▒░░░░░░░░░░░░░░    ░░░░░░░░    
+   ░▓▓▒▒▒▒░░░░░░░░░░░░        ░░░░░░░    
+  ░▓▓▓▒▒▒▒░░░░░░░░░░░░        ░░░░░░░░  
+  ░▓▓▓▒▒▒▒░░░░░░░░░░░░░░    ░░░░░░░░░░  
+  ░▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░  
+  ░▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░  
+   ░▓▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░  
+    ░░▓▓▓▓▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░    
+    ░░▓▓▓▓▓▓▒▒▒▒▒▒▒▒░░░░░░░░░▒▒▒▒▒░░    
+      ░░▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░      
+          ░░▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░          
+
+               OURS TRULY,
+
+
+    github.com/ourzora/zora-1155-contracts
+
+*/
+
+/// @title FixedPriceAllowedMintersStrategy
+/// @notice A sale strategy for fixed price sales from an allowed set of minters
+contract FixedPriceAllowedMintersStrategy is Enjoy, SaleStrategy, LimitedMintPerAddress, IMinterErrors {
+    using SaleCommandHelper for ICreatorCommands.CommandSet;
+
+    event SaleSet(address indexed mediaContract, uint256 indexed tokenId, SalesConfig salesConfig);
+    event MintComment(address indexed sender, address indexed tokenContract, uint256 indexed tokenId, uint256 quantity, string comment);
+    event MinterSet(address indexed mediaContract, uint256 indexed tokenId, address indexed minter, bool allowed);
+
+    error ONLY_MINTER();
+
+    struct SalesConfig {
+        /// @notice Unix timestamp for the sale start
+        uint64 saleStart;
+        /// @notice Unix timestamp for the sale end
+        uint64 saleEnd;
+        /// @notice Max tokens that can be minted for an address, 0 if unlimited
+        uint64 maxTokensPerAddress;
+        /// @notice Price per token in eth wei
+        uint96 pricePerToken;
+        /// @notice Funds recipient (0 if no different funds recipient than the contract global)
+        address fundsRecipient;
+    }
+
+    // 1155 contract -> 1155 tokenId -> settings
+    mapping(address => mapping(uint256 => SalesConfig)) internal salesConfigs;
+
+    // 1155 contract => 1155 tokenId => minter address => allowed
+    mapping(address => mapping(uint256 => mapping(address => bool))) internal allowedMinters;
+
+    /// @notice If a minter address is allowed to mint a token
+    function isMinter(address tokenContract, uint256 tokenId, address minter) public view returns (bool) {
+        if (allowedMinters[tokenContract][tokenId][minter] || allowedMinters[tokenContract][0][minter]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /// @notice Sets the allowed addresses that can mint a given token
+    /// @param tokenId The tokenId to set the minters for OR tokenId 0 to set the minters for all tokens contract-wide
+    function setMinters(uint256 tokenId, address[] calldata minters) external {
+        uint256 numMinters = minters.length;
+
+        for (uint256 i; i < numMinters; ++i) {
+            allowedMinters[msg.sender][tokenId][minters[i]] = true;
+
+            emit MinterSet(msg.sender, tokenId, minters[i], true);
+        }
+    }
+
+    /// @notice Remove the given allowed addresses from being able to mint a given token
+    function removeMinters(uint256 tokenId, address[] calldata minters) external {
+        uint256 numMinters = minters.length;
+
+        for (uint256 i; i < numMinters; ++i) {
+            allowedMinters[msg.sender][tokenId][minters[i]] = false;
+
+            emit MinterSet(msg.sender, tokenId, minters[i], false);
+        }
+    }
+
+    /// @notice Sets the sale config for a given token
+    function setSale(uint256 tokenId, SalesConfig calldata salesConfig) external {
+        salesConfigs[msg.sender][tokenId] = salesConfig;
+
+        emit SaleSet(msg.sender, tokenId, salesConfig);
+    }
+
+    /// @notice Deletes the sale config for a given token
+    function resetSale(uint256 tokenId) external override {
+        delete salesConfigs[msg.sender][tokenId];
+
+        // Deleted sale emit event
+        emit SaleSet(msg.sender, tokenId, salesConfigs[msg.sender][tokenId]);
+    }
+
+    /// @notice Compiles and returns the commands needed to mint a token using this sales strategy
+    /// @param mintMsgSender The address that called the mint on the 1155 contract
+    /// @param tokenId The token ID to mint
+    /// @param quantity The quantity of tokens to mint
+    /// @param ethValueSent The amount of ETH sent with the transaction
+    /// @param minterArguments The arguments passed to the minter, which should be the address to mint to
+    function requestMint(
+        address mintMsgSender,
+        uint256 tokenId,
+        uint256 quantity,
+        uint256 ethValueSent,
+        bytes calldata minterArguments
+    ) external returns (ICreatorCommands.CommandSet memory commands) {
+        // Ensure the minter is allowed to mint either this token or all tokens contract-wide
+        if (!isMinter(msg.sender, tokenId, mintMsgSender)) {
+            revert ONLY_MINTER();
+        }
+
+        address mintTo;
+        string memory comment = "";
+
+        if (minterArguments.length == 32) {
+            mintTo = abi.decode(minterArguments, (address));
+        } else {
+            (mintTo, comment) = abi.decode(minterArguments, (address, string));
+        }
+
+        SalesConfig storage config = salesConfigs[msg.sender][tokenId];
+
+        // If sales config does not exist this first check will always fail.
+
+        // Check sale end
+        if (block.timestamp > config.saleEnd) {
+            revert SaleEnded();
+        }
+
+        // Check sale start
+        if (block.timestamp < config.saleStart) {
+            revert SaleHasNotStarted();
+        }
+
+        // Check value sent
+        if (config.pricePerToken * quantity != ethValueSent) {
+            revert WrongValueSent();
+        }
+
+        bool shouldTransferFunds = config.fundsRecipient != address(0);
+        commands.setSize(shouldTransferFunds ? 2 : 1);
+
+        // Mint command
+        commands.mint(mintTo, tokenId, quantity);
+
+        if (bytes(comment).length > 0) {
+            emit MintComment(mintTo, msg.sender, tokenId, quantity, comment);
+        }
+
+        // Should transfer funds if funds recipient is set to a non-default address
+        if (shouldTransferFunds) {
+            commands.transfer(config.fundsRecipient, ethValueSent);
+        }
+    }
+
+
+    /// @notice Returns the sale config for a given token
+    function sale(address tokenContract, uint256 tokenId) external view returns (SalesConfig memory) {
+        return salesConfigs[tokenContract][tokenId];
+    }
+
+    /// @notice The version of the sale strategy
+    function contractVersion() external pure override returns (string memory) {
+        return "1.0.0";
+    }
+
+    function contractURI() external pure override returns (string memory) {
+        return "https://github.com/ourzora/zora-1155-contracts/";
+    }
+
+    /// @notice The name of the sale strategy
+    function contractName() external pure override returns (string memory) {
+        return "Fixed Price Allowed Minters Strategy";
+    }
+
+    function supportsInterface(bytes4 interfaceId) public pure virtual override(LimitedMintPerAddress, SaleStrategy) returns (bool) {
+        return super.supportsInterface(interfaceId) || LimitedMintPerAddress.supportsInterface(interfaceId) || SaleStrategy.supportsInterface(interfaceId);
+    }
+}
---
+++ packages/1155-contracts/test/minters/fixed-price/FixedPriceAllowedMintersStrategy.t.sol
@@ -0,0 +1,210 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+
+import {ProtocolRewards} from "@zoralabs/protocol-rewards/src/ProtocolRewards.sol";
+import {ZoraCreator1155Impl} from "../../../src/nft/ZoraCreator1155Impl.sol";
+import {Zora1155} from "../../../src/proxies/Zora1155.sol";
+import {IZoraCreator1155Errors} from "../../../src/interfaces/IZoraCreator1155Errors.sol";
+import {IMinter1155} from "../../../src/interfaces/IMinter1155.sol";
+import {ICreatorRoyaltiesControl} from "../../../src/interfaces/ICreatorRoyaltiesControl.sol";
+import {IZoraCreator1155Factory} from "../../../src/interfaces/IZoraCreator1155Factory.sol";
+import {ILimitedMintPerAddressErrors} from "../../../src/interfaces/ILimitedMintPerAddress.sol";
+
+import {FixedPriceAllowedMintersStrategy} from "../../../src/minters/fixed-price/FixedPriceAllowedMintersStrategy.sol";
+
+contract FixedPriceAllowedMintersStrategyTest is Test {
+    ZoraCreator1155Impl internal targetImpl;
+    ZoraCreator1155Impl internal target;
+    FixedPriceAllowedMintersStrategy internal fixedPrice;
+
+    address payable internal admin;
+    address internal zora;
+    address internal tokenRecipient;
+    address internal fundsRecipient;
+
+    address internal allowedMinter;
+    address[] internal minters;
+
+    event SaleSet(
+        address indexed mediaContract, uint256 indexed tokenId, FixedPriceAllowedMintersStrategy.SalesConfig salesConfig
+    );
+    event MintComment(
+        address indexed sender, address indexed tokenContract, uint256 indexed tokenId, uint256 quantity, string comment
+    );
+
+    function setUp() external {
+        admin = payable(makeAddr("admin"));
+        zora = makeAddr("zora");
+        tokenRecipient = makeAddr("tokenRecipient");
+        fundsRecipient = makeAddr("fundsRecipient");
+
+        allowedMinter = makeAddr("allowedMinter");
+        minters = new address[](1);
+        minters[0] = allowedMinter;
+
+        targetImpl = new ZoraCreator1155Impl(zora, address(0), address(new ProtocolRewards()));
+        target = ZoraCreator1155Impl(address(new Zora1155(address(targetImpl))));
+
+        target.initialize(
+            "test", "test", ICreatorRoyaltiesControl.RoyaltyConfiguration(0, 0, address(0)), admin, new bytes[](0)
+        );
+        fixedPrice = new FixedPriceAllowedMintersStrategy();
+    }
+
+    function test_ContractName() external {
+        assertEq(fixedPrice.contractName(), "Fixed Price Allowed Minters Strategy");
+    }
+
+    function test_Version() external {
+        assertEq(fixedPrice.contractVersion(), "1.0.0");
+    }
+
+    function test_MintFromAllowedMinter() external {
+        vm.startPrank(admin);
+
+        uint256 newTokenId = target.setupNewToken("https://zora.co/testing/token.json", 10);
+        target.addPermission(newTokenId, address(fixedPrice), target.PERMISSION_BIT_MINTER());
+
+        target.callSale(
+            newTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(
+                FixedPriceAllowedMintersStrategy.setSale.selector,
+                newTokenId,
+                FixedPriceAllowedMintersStrategy.SalesConfig({
+                    pricePerToken: 1 ether,
+                    saleStart: 0,
+                    saleEnd: type(uint64).max,
+                    maxTokensPerAddress: 0,
+                    fundsRecipient: address(0)
+                })
+            )
+        );
+
+        target.callSale(
+            newTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(FixedPriceAllowedMintersStrategy.setMinters.selector, newTokenId, minters)
+        );
+
+        vm.stopPrank();
+
+        uint256 numTokens = 10;
+        uint256 totalReward = target.computeTotalReward(numTokens);
+        uint256 totalValue = (1 ether * numTokens) + totalReward;
+        vm.deal(allowedMinter, totalValue);
+
+        vm.startPrank(allowedMinter);
+        target.mint{value: totalValue}(fixedPrice, newTokenId, 10, abi.encode(tokenRecipient, "test comment"));
+
+        assertEq(target.balanceOf(tokenRecipient, newTokenId), 10);
+        assertEq(address(target).balance, 10 ether);
+
+        vm.stopPrank();
+    }
+
+    function test_MintFromAllowedMinterContractWide() external {
+        vm.startPrank(admin);
+
+        uint256 newTokenId = target.setupNewToken("https://zora.co/testing/token.json", 10);
+        uint256 newNewTokenId = target.setupNewToken("https://zora.co/testing/token.json", 20);
+
+        target.addPermission(0, address(fixedPrice), target.PERMISSION_BIT_MINTER());
+        target.addPermission(newTokenId, address(fixedPrice), target.PERMISSION_BIT_MINTER());
+        target.addPermission(newNewTokenId, address(fixedPrice), target.PERMISSION_BIT_MINTER());
+
+        target.callSale(
+            newTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(
+                FixedPriceAllowedMintersStrategy.setSale.selector,
+                newTokenId,
+                FixedPriceAllowedMintersStrategy.SalesConfig({
+                    pricePerToken: 1 ether,
+                    saleStart: 0,
+                    saleEnd: type(uint64).max,
+                    maxTokensPerAddress: 0,
+                    fundsRecipient: address(0)
+                })
+            )
+        );
+
+        target.callSale(
+            newNewTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(
+                FixedPriceAllowedMintersStrategy.setSale.selector,
+                newNewTokenId,
+                FixedPriceAllowedMintersStrategy.SalesConfig({
+                    pricePerToken: 1 ether,
+                    saleStart: 0,
+                    saleEnd: type(uint64).max,
+                    maxTokensPerAddress: 0,
+                    fundsRecipient: address(0)
+                })
+            )
+        );
+
+        target.callSale(
+            0, fixedPrice, abi.encodeWithSelector(FixedPriceAllowedMintersStrategy.setMinters.selector, 0, minters)
+        );
+
+        vm.stopPrank();
+
+        uint256 numTokens = 10;
+        uint256 totalReward = target.computeTotalReward(numTokens);
+        uint256 totalValue = (1 ether * numTokens) + totalReward;
+        vm.deal(allowedMinter, totalValue * 2);
+
+        vm.startPrank(allowedMinter);
+        target.mint{value: totalValue}(fixedPrice, newTokenId, 10, abi.encode(tokenRecipient, "test comment"));
+        target.mint{value: totalValue}(fixedPrice, newNewTokenId, 10, abi.encode(tokenRecipient, "test comment"));
+
+        assertEq(target.balanceOf(tokenRecipient, newTokenId), 10);
+        assertEq(target.balanceOf(tokenRecipient, newNewTokenId), 10);
+        assertEq(address(target).balance, 20 ether);
+
+        vm.stopPrank();
+    }
+
+    function testRevert_MinterNotAllowed() external {
+        vm.startPrank(admin);
+
+        uint256 newTokenId = target.setupNewToken("https://zora.co/testing/token.json", 10);
+        target.addPermission(newTokenId, address(fixedPrice), target.PERMISSION_BIT_MINTER());
+
+        target.callSale(
+            newTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(
+                FixedPriceAllowedMintersStrategy.setSale.selector,
+                newTokenId,
+                FixedPriceAllowedMintersStrategy.SalesConfig({
+                    pricePerToken: 1 ether,
+                    saleStart: 0,
+                    saleEnd: type(uint64).max,
+                    maxTokensPerAddress: 0,
+                    fundsRecipient: address(0)
+                })
+            )
+        );
+
+        target.callSale(
+            newTokenId,
+            fixedPrice,
+            abi.encodeWithSelector(FixedPriceAllowedMintersStrategy.setMinters.selector, newTokenId, minters)
+        );
+
+        vm.stopPrank();
+
+        uint256 numTokens = 10;
+        uint256 totalReward = target.computeTotalReward(numTokens);
+        uint256 totalValue = (1 ether * numTokens) + totalReward;
+        vm.deal(allowedMinter, totalValue);
+
+        vm.expectRevert(abi.encodeWithSignature("ONLY_MINTER()"));
+        target.mint{value: totalValue}(fixedPrice, newTokenId, 10, abi.encode(tokenRecipient, "test comment"));
+    }
+}
