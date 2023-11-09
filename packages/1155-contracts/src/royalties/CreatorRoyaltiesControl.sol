ourzora/zora-protocol

commit b1524b641778c9db4941195287b784f21f8f6294

Included all errors in erc1155 and preminter abis (#313)

packages/1155-contracts/src/royalties/CreatorRoyaltiesControl.sol
@@ -4,13 +4,14 @@ pragma solidity 0.8.17;
 import {CreatorRoyaltiesStorageV1} from "./CreatorRoyaltiesStorageV1.sol";
 import {ICreatorRoyaltiesControl} from "../interfaces/ICreatorRoyaltiesControl.sol";
 import {SharedBaseConstants} from "../shared/SharedBaseConstants.sol";
+import {ICreatorRoyaltyErrors} from "../interfaces/ICreatorRoyaltiesControl.sol";
 import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 /// Imagine. Mint. Enjoy.
 /// @title CreatorRoyaltiesControl
 /// @author ZORA @iainnash / @tbtstl
 /// @notice Contract for managing the royalties of an 1155 contract
-abstract contract CreatorRoyaltiesControl is CreatorRoyaltiesStorageV1, SharedBaseConstants {
+abstract contract CreatorRoyaltiesControl is CreatorRoyaltiesStorageV1, SharedBaseConstants, ICreatorRoyaltyErrors {
     uint256 immutable ROYALTY_BPS_TO_PERCENT = 10_000;
 
     /// @notice The royalty information for a given token.

packages/1155-contracts/src/interfaces/ICreatorRoyaltiesControl.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.17;
 
 import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
+interface ICreatorRoyaltyErrors {
+    /// @notice Thrown when a user tries to have 100% supply royalties
+    error InvalidMintSchedule();
+}
+
 interface ICreatorRoyaltiesControl is IERC2981 {
     /// @notice The RoyaltyConfiguration struct is used to store the royalty configuration for a given token.
     /// @param royaltyMintSchedule Every nth token will go to the royalty recipient.
@@ -13,9 +18,6 @@ interface ICreatorRoyaltiesControl is IERC2981 {
         uint32 royaltyBPS;
         address royaltyRecipient;
     }
-
-    /// @notice Thrown when a user tries to have 100% supply royalties
-    error InvalidMintSchedule();
 
     /// @notice Event emitted when royalties are updated
     event UpdatedRoyalties(uint256 indexed tokenId, address indexed user, RoyaltyConfiguration configuration);

packages/1155-contracts/src/interfaces/ILimitedMintPerAddress.sol
@@ -3,8 +3,10 @@ pragma solidity 0.8.17;
 
 import {IERC165Upgradeable} from "@zoralabs/openzeppelin-contracts-upgradeable/contracts/interfaces/IERC165Upgradeable.sol";
 
-interface ILimitedMintPerAddress is IERC165Upgradeable {
+interface ILimitedMintPerAddressErrors {
     error UserExceedsMintLimit(address user, uint256 limit, uint256 requestedAmount);
+}
 
+interface ILimitedMintPerAddress is IERC165Upgradeable, ILimitedMintPerAddressErrors {
     function getMintedPerWallet(address token, uint256 tokenId, address wallet) external view returns (uint256);
 }

packages/1155-contracts/src/interfaces/IMinterErrors.sol
@@ -1,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IMinterErrors {
+    error CallerNotZoraCreator1155();
+    error MinterContractAlreadyExists();
+    error MinterContractDoesNotExist();
+
+    error SaleEnded();
+    error SaleHasNotStarted();
+    error WrongValueSent();
+    error InvalidMerkleProof(address mintTo, bytes32[] merkleProof, bytes32 merkleRoot);
+}

packages/1155-contracts/src/interfaces/IZoraCreator1155Errors.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IZoraCreator1155Errors {
+import {ICreatorRoyaltyErrors} from "./ICreatorRoyaltiesControl.sol";
+import {ILimitedMintPerAddressErrors} from "./ILimitedMintPerAddress.sol";
+import {IMinterErrors} from "./IMinterErrors.sol";
+
+interface IZoraCreator1155Errors is ICreatorRoyaltyErrors, ILimitedMintPerAddressErrors, IMinterErrors {
     error Call_TokenIdMismatch();
     error TokenIdMismatch(uint256 expected, uint256 actual);
     error UserMissingRoleForToken(address user, uint256 tokenId, uint256 role);

packages/1155-contracts/src/minters/fixed-price/ZoraCreatorFixedPriceSaleStrategy.sol
@@ -7,6 +7,7 @@ import {ICreatorCommands} from "../../interfaces/ICreatorCommands.sol";
 import {SaleStrategy} from "../SaleStrategy.sol";
 import {SaleCommandHelper} from "../utils/SaleCommandHelper.sol";
 import {LimitedMintPerAddress} from "../utils/LimitedMintPerAddress.sol";
+import {IMinterErrors} from "../../interfaces/IMinterErrors.sol";
 
 /*
 
@@ -36,7 +37,7 @@ import {LimitedMintPerAddress} from "../utils/LimitedMintPerAddress.sol";
 /// @title ZoraCreatorFixedPriceSaleStrategy
 /// @notice A sale strategy for ZoraCreator that allows for fixed price sales over a given time period
 /// @author @iainnash / @tbtstl
-contract ZoraCreatorFixedPriceSaleStrategy is Enjoy, SaleStrategy, LimitedMintPerAddress {
+contract ZoraCreatorFixedPriceSaleStrategy is Enjoy, SaleStrategy, LimitedMintPerAddress, IMinterErrors {
     struct SalesConfig {
         /// @notice Unix timestamp for the sale start
         uint64 saleStart;
@@ -68,10 +69,6 @@ contract ZoraCreatorFixedPriceSaleStrategy is Enjoy, SaleStrategy, LimitedMintPe
     function contractVersion() external pure override returns (string memory) {
         return "1.1.0";
     }
-
-    error WrongValueSent();
-    error SaleEnded();
-    error SaleHasNotStarted();
 
     event SaleSet(address indexed mediaContract, uint256 indexed tokenId, SalesConfig salesConfig);
     event MintComment(address indexed sender, address indexed tokenContract, uint256 indexed tokenId, uint256 quantity, string comment);

packages/1155-contracts/src/minters/merkle/ZoraCreatorMerkleMinterStrategy.sol
@@ -9,6 +9,7 @@ import {SaleStrategy} from "../SaleStrategy.sol";
 import {ICreatorCommands} from "../../interfaces/ICreatorCommands.sol";
 import {SaleCommandHelper} from "../utils/SaleCommandHelper.sol";
 import {LimitedMintPerAddress} from "../utils/LimitedMintPerAddress.sol";
+import {IMinterErrors} from "../../interfaces/IMinterErrors.sol";
 
 /*
 
@@ -38,7 +39,7 @@ import {LimitedMintPerAddress} from "../utils/LimitedMintPerAddress.sol";
 /// @title ZoraCreatorMerkleMinterStrategy
 /// @notice Mints tokens based on a merkle tree, for presales for example
 /// @author @iainnash / @tbtstl
-contract ZoraCreatorMerkleMinterStrategy is Enjoy, SaleStrategy, LimitedMintPerAddress {
+contract ZoraCreatorMerkleMinterStrategy is Enjoy, SaleStrategy, LimitedMintPerAddress, IMinterErrors {
     using SaleCommandHelper for ICreatorCommands.CommandSet;
 
     /// @notice General merkle sale settings
@@ -58,12 +59,6 @@ contract ZoraCreatorMerkleMinterStrategy is Enjoy, SaleStrategy, LimitedMintPerA
 
     /// @notice Storage for allowed merkle settings for the sales configuration
     mapping(address => mapping(uint256 => MerkleSaleSettings)) public allowedMerkles;
-    // target -> tokenId -> settings
-
-    error SaleEnded();
-    error SaleHasNotStarted();
-    error WrongValueSent();
-    error InvalidMerkleProof(address mintTo, bytes32[] merkleProof, bytes32 merkleRoot);
 
     /// @notice ContractURI for contract information with the strategy
     function contractURI() external pure override returns (string memory) {

packages/1155-contracts/src/minters/redeem/ZoraCreatorRedeemMinterFactory.sol
@@ -12,6 +12,7 @@ import {ICreatorCommands} from "../../interfaces/ICreatorCommands.sol";
 import {ZoraCreatorRedeemMinterStrategy} from "./ZoraCreatorRedeemMinterStrategy.sol";
 import {IZoraCreator1155} from "../../interfaces/IZoraCreator1155.sol";
 import {SharedBaseConstants} from "../../shared/SharedBaseConstants.sol";
+import {IMinterErrors} from "../../interfaces/IMinterErrors.sol";
 
 /*
 
@@ -41,15 +42,11 @@ import {SharedBaseConstants} from "../../shared/SharedBaseConstants.sol";
 /// @title ZoraCreatorRedeemMinterFactory
 /// @notice A factory for ZoraCreatorRedeemMinterStrategy contracts
 /// @author @jgeary
-contract ZoraCreatorRedeemMinterFactory is Enjoy, IContractMetadata, SharedBaseConstants, IVersionedContract, IMinter1155 {
+contract ZoraCreatorRedeemMinterFactory is Enjoy, IContractMetadata, SharedBaseConstants, IVersionedContract, IMinter1155, IMinterErrors {
     bytes4 constant LEGACY_ZORA_IMINTER1155_INTERFACE_ID = 0x6467a6fc;
     address public immutable zoraRedeemMinterImplementation;
 
     event RedeemMinterDeployed(address indexed creatorContract, address indexed minterContract);
-
-    error CallerNotZoraCreator1155();
-    error MinterContractAlreadyExists();
-    error MinterContractDoesNotExist();
 
     constructor() {
         zoraRedeemMinterImplementation = address(new ZoraCreatorRedeemMinterStrategy());

commit 366ac207ceae8a68a9365600c2d1f50385b0faf5

Fix broken storage layout issue as a result of #313 (#330)
Storage layout was broken in #313.  This removes an interface inheritance that broke it before

packages/1155-contracts/src/royalties/CreatorRoyaltiesControl.sol
@@ -11,7 +11,7 @@ import {IERC2981} from "@openzeppelin/contracts/interfaces/IERC2981.sol";
 /// @title CreatorRoyaltiesControl
 /// @author ZORA @iainnash / @tbtstl
 /// @notice Contract for managing the royalties of an 1155 contract
-abstract contract CreatorRoyaltiesControl is CreatorRoyaltiesStorageV1, SharedBaseConstants, ICreatorRoyaltyErrors {
+abstract contract CreatorRoyaltiesControl is CreatorRoyaltiesStorageV1, SharedBaseConstants {
     uint256 immutable ROYALTY_BPS_TO_PERCENT = 10_000;
 
     /// @notice The royalty information for a given token.
@@ -41,7 +41,7 @@ abstract contract CreatorRoyaltiesControl is CreatorRoyaltiesStorageV1, SharedBa
         }
         // Don't allow setting royalties to burn address
         if (configuration.royaltyRecipient == address(0) && configuration.royaltyBPS > 0) {
-            revert InvalidMintSchedule();
+            revert ICreatorRoyaltyErrors.InvalidMintSchedule();
         }
         royalties[tokenId] = configuration;
 