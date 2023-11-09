a50ae1c7e577723bb6d0f914fb03eb31a67e8541
Dan Oved <stangogh@gmail.com>
2023-11-09 12:51:48 -0800

    Move deployment related config to protocol-deployments

--- packages/protocol-deployments/src/DeterministicProxyDeployer.sol
+++ packages/protocol-deployments/src/DeterministicProxyDeployer.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
-import {ProxyShim} from "../utils/ProxyShim.sol";
+import {ProxyShim} from "@zoralabs/zora-1155-contracts/src/utils/ProxyShim.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
