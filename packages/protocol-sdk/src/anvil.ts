e72e8f55c71cb7246df1fc202559cf083b64eea0
Dan Oved <stangogh@gmail.com>
2023-11-13 11:16:29 -0800

    Protocol SDK (#348)
    * added back build-js task
    
    * Add protocol sdk and minting features (#322)
    
    * Moved premint sdk to protocol sdk
    
    * add protocol sdk and minting features
    
    ---------
    
    Co-authored-by: Dan Oved <stangogh@gmail.com>
    
    ---------
    
    Co-authored-by: Iain Nash <iain@zora.co>

---
+++ packages/protocol-sdk/src/anvil.ts
@@ -0,0 +1,84 @@
+import { spawn } from "node:child_process";
+import { join } from "path";
+import { test } from "vitest";
+import {
+  PublicClient,
+  TestClient,
+  WalletClient,
+  createPublicClient,
+  createTestClient,
+  createWalletClient,
+  http,
+} from "viem";
+import { foundry } from "viem/chains";
+
+export interface AnvilViemClientsTest {
+  viemClients: {
+    walletClient: WalletClient;
+    publicClient: PublicClient;
+    testClient: TestClient;
+  };
+}
+
+async function waitForAnvilInit(anvil: any) {
+  return new Promise((resolve) => {
+    anvil.stdout.once("data", () => {
+      resolve(true);
+    });
+  });
+}
+
+export const anvilTest = test.extend<AnvilViemClientsTest>({
+  viemClients: async ({task}, use) => {
+    console.log('setting up clients for ', task.name);
+    const port = Math.floor(Math.random() * 2000) + 4000;
+    const anvil = spawn(
+      "anvil",
+      [
+        "--port",
+        `${port}`,
+        "--fork-url",
+        "https://rpc.zora.co/",
+        "--fork-block-number",
+        "6133407",
+        "--chain-id",
+        "31337",
+      ],
+      {
+        cwd: join(__dirname, ".."),
+        killSignal: "SIGINT",
+      },
+    );
+    const anvilHost = `http://0.0.0.0:${port}`;
+    await waitForAnvilInit(anvil);
+
+    const chain = {
+      ...foundry,
+    };
+
+    const walletClient = createWalletClient({
+      chain,
+      transport: http(anvilHost),
+    });
+
+    const testClient = createTestClient({
+      chain,
+      mode: "anvil",
+      transport: http(anvilHost),
+    });
+
+    const publicClient = createPublicClient({
+      chain,
+      transport: http(anvilHost),
+    });
+
+    await use({
+      publicClient,
+      walletClient,
+      testClient,
+    });
+
+    // clean up function, called once after all tests run
+    anvil.kill("SIGINT");
+  },
+});
---
+++ packages/protocol-sdk/src/mint/mint-client.test.ts
@@ -0,0 +1,117 @@
+import { parseAbi, parseEther } from "viem";
+import { zora } from "viem/chains";
+import { describe, expect } from "vitest";
+import { MintClient } from "./mint-client";
+import { zoraCreator1155ImplABI } from "@zoralabs/protocol-deployments";
+import { anvilTest } from "src/anvil";
+
+const erc721ABI = parseAbi([
+  "function balanceOf(address owner) public view returns (uint256)",
+] as const);
+
+describe("mint-helper", () => {
+  anvilTest(
+    "mints a new 1155 token",
+    async ({ viemClients }) => {
+      const { testClient, walletClient, publicClient } = viemClients;
+      const creatorAccount = (await walletClient.getAddresses())[0]!;
+      await testClient.setBalance({
+        address: creatorAccount,
+        value: parseEther("2000"),
+      });
+      const targetContract = "0xa2fea3537915dc6c7c7a97a82d1236041e6feb2e";
+      const targetTokenId = 1n;
+      const minter = new MintClient(zora);
+
+      const { simulateContractParameters: params } =
+        await minter.makePrepareMintTokenParams({
+          publicClient,
+          minterAccount: creatorAccount,
+          mintable: await minter.getMintable({
+            tokenId: targetTokenId,
+            tokenContract: targetContract,
+          }),
+          mintArguments: {
+            mintToAddress: creatorAccount,
+            quantityToMint: 1,
+          },
+        });
+
+      const oldBalance = await publicClient.readContract({
+        abi: zoraCreator1155ImplABI,
+        address: targetContract,
+        functionName: "balanceOf",
+        args: [creatorAccount, targetTokenId],
+      });
+
+      const simulationResult = await publicClient.simulateContract(params);
+
+      const hash = await walletClient.writeContract(simulationResult.request);
+      const receipt = await publicClient.waitForTransactionReceipt({ hash });
+      const newBalance = await publicClient.readContract({
+        abi: zoraCreator1155ImplABI,
+        address: targetContract,
+        functionName: "balanceOf",
+        args: [creatorAccount, targetTokenId],
+      });
+      expect(receipt).to.not.be.null;
+      expect(oldBalance).to.be.equal(0n);
+      expect(newBalance).to.be.equal(1n);
+    },
+    12 * 1000,
+  );
+
+  anvilTest(
+    "mints a new 721 token",
+    async ({ viemClients }) => {
+      const { testClient, walletClient, publicClient } = viemClients;
+      const creatorAccount = (await walletClient.getAddresses())[0]!;
+      await testClient.setBalance({
+        address: creatorAccount,
+        value: parseEther("2000"),
+      });
+
+      const targetContract = "0x7aae7e67515A2CbB8585C707Ca6db37BDd3EA839";
+      const targetTokenId = undefined;
+      const minter = new MintClient(zora);
+
+      const { simulateContractParameters: prepared } =
+        await minter.makePrepareMintTokenParams({
+          mintable: await minter.getMintable({
+            tokenContract: targetContract,
+            tokenId: targetTokenId,
+          }),
+          publicClient,
+          minterAccount: creatorAccount,
+          mintArguments: {
+            mintToAddress: creatorAccount,
+            quantityToMint: 1,
+          },
+        });
+      const oldBalance = await publicClient.readContract({
+        abi: erc721ABI,
+        address: targetContract,
+        functionName: "balanceOf",
+        args: [creatorAccount],
+      });
+
+      const simulated = await publicClient.simulateContract(prepared);
+
+      const hash = await walletClient.writeContract(simulated.request);
+
+      const receipt = await publicClient.getTransactionReceipt({ hash });
+      expect(receipt).not.to.be.null;
+
+      const newBalance = await publicClient.readContract({
+        abi: erc721ABI,
+        address: targetContract,
+        functionName: "balanceOf",
+        args: [creatorAccount],
+      });
+
+      expect(oldBalance).to.be.equal(0n);
+      expect(newBalance).to.be.equal(1n);
+    },
+    12 * 1000,
+  );
+});
