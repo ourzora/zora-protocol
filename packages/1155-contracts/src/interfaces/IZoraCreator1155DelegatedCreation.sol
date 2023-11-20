dd9e130311572e73c11208b5fd6c9ad8c4cbb954
iain nash <me@iain.in>
2023-11-20 14:29:24 -0500

    remove accidental changes

--- packages/1155-contracts/src/interfaces/IZoraCreator1155DelegatedCreation.sol
+++ packages/1155-contracts/src/interfaces/IZoraCreator1155DelegatedCreation.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.17;
 interface IZoraCreator1155DelegatedCreation {
     event CreatorAttribution(bytes32 structHash, string domainName, string version, address creator, bytes signature);
 
-    function supportedPremintSignatureVersions() external pure returns (string[] memory);
-
     function delegateSetupNewToken(
         bytes memory premintConfigEncoded,
         bytes32 premintVersion,
