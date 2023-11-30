2831570a1ee6480a0fea751267c59967210e0315
Rohan Kulkarni <kulkarohan@gmail.com>
2023-11-30 11:16:25 -0500

    chore: initial tests

---
+++ packages/zora-account/test/ZoraAccountFactory.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./ZoraAccountTestSetup.sol";
+
+contract ZoraAccountFactoryTest is ZoraAccountTestSetup {
+    ZoraAccountImpl internal account;
+
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function testFactoryGetAddress(address fuzzOwner, uint256 fuzzOwnerSalt) public {
+        address precomputedAccountAddress = zoraAccountFactory.getAddress(fuzzOwner, fuzzOwnerSalt);
+
+        account = deployAccount(fuzzOwner, fuzzOwnerSalt);
+
+        assertEq(address(account), precomputedAccountAddress);
+    }
+
+    function testFactoryGetAddressWhenAccountExists(address fuzzOwner, uint256 fuzzOwnerSalt) public {
+        account = deployAccount(fuzzOwner, fuzzOwnerSalt);
+
+        address precomputedAccountAddress = zoraAccountFactory.getAddress(fuzzOwner, fuzzOwnerSalt);
+
+        assertEq(address(account), precomputedAccountAddress);
+    }
+
+    function testFactoryImpl() public {
+        assertEq(address(zoraAccountFactoryImpl), zoraAccountFactory.implementation());
+    }
+
+    function testFactoryAccountImpl() public {
+        account = deployAccount(accountOwnerEOA, uint256(accountOwnerSalt));
+
+        assertEq(account.implementation(), address(zoraAccountFactory.zoraAccountImpl()));
+    }
+}
---
+++ packages/zora-account/test/ZoraAccountTestSetup.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import {IEntryPoint, EntryPoint} from "account-abstraction/contracts/core/EntryPoint.sol";
+import {UserOperation} from "account-abstraction/contracts/interfaces/UserOperation.sol";
+
+import {ZoraAccountFactoryImpl} from "../src/factory/ZoraAccountFactoryImpl.sol";
+import {ZoraAccountImpl} from "../src/account/ZoraAccountImpl.sol";
+import {ZoraAccountFactory} from "../src/proxy/ZoraAccountFactory.sol";
+import {ZoraAccount} from "../src/proxy/ZoraAccount.sol";
+
+contract ZoraAccountTestSetup is Test {
+    address internal accountOwnerEOA;
+    uint256 internal accountOwnerPK;
+    bytes32 internal accountOwnerSalt;
+    address internal zora;
+
+    EntryPoint internal entryPoint;
+    ZoraAccountFactoryImpl internal zoraAccountFactoryImpl;
+    ZoraAccountFactoryImpl internal zoraAccountFactory;
+
+    function setUp() public virtual {
+        (accountOwnerEOA, accountOwnerPK) = makeAddrAndKey("accountOwnerEOA");
+        accountOwnerSalt = keccak256(abi.encodePacked("accountOwnerSalt"));
+        zora = makeAddr("zora");
+
+        entryPoint = new EntryPoint();
+        zoraAccountFactoryImpl = new ZoraAccountFactoryImpl(entryPoint);
+        zoraAccountFactory = ZoraAccountFactoryImpl(
+            payable(
+                address(
+                    new ZoraAccountFactory(address(zoraAccountFactoryImpl), abi.encodeWithSelector(zoraAccountFactoryImpl.initialize.selector, zora))
+                )
+            )
+        );
+    }
+
+    function deployAccount(address owner, uint256 salt) internal returns (ZoraAccountImpl) {
+        return ZoraAccountImpl(payable(address(zoraAccountFactory.createAccount(owner, salt))));
+    }
+}
