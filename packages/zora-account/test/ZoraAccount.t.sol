f7447fa4f2a62c66d6df8dc5ea3f51f47c261098
Isabella Smallcombe <isabella@ourzora.com>
2023-12-01 18:06:56 -0500

    feat: add tests for zora account

--- packages/zora-account/test/ZoraAccount.t.sol
+++ packages/zora-account/test/ZoraAccount.t.sol
@@ -12,7 +12,16 @@ contract ZoraAccountTest is ZoraAccountTestSetup {
         account = deployAccount(accountOwnerEOA, uint256(accountOwnerSalt));
     }
 
-    function testExecuteViaOwner() public {}
+    function testExecuteViaOwner() public {
+        vm.prank(accountOwnerEOA);
+        account.execute(address(0), 0, "");
+    }
 
-    function testExecuteWithValueViaOwner() public {}
+    function testExecuteWithValueViaOwner() public {
+        address testAddress = makeAddr("test");
+        vm.deal(address(account), 1 ether);
+        vm.prank(accountOwnerEOA);
+        account.execute(testAddress, 1 ether, "");
+        assertEq(testAddress.balance, 1 ether);
+    }
 }
