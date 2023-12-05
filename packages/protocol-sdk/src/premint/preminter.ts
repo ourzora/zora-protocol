6abc20aa3a5399d9b606d74f4e5c6c3a95186246
Dan Oved <stangogh@gmail.com>
2023-12-05 12:07:39 -0800

    Premint v2 sdk - move types to their own file (#16)

--- packages/protocol-sdk/src/premint/preminter.ts
+++ packages/protocol-sdk/src/premint/preminter.ts
@@ -1,5 +1,4 @@
 import { Address } from "abitype";
-import { ExtractAbiFunction, AbiParametersToPrimitiveTypes } from "abitype";
 import {
   zoraCreator1155PremintExecutorImplABI as preminterAbi,
   zoraCreator1155ImplABI,
@@ -18,102 +17,16 @@ import {
   recoverAddress,
   GetEventArgs,
 } from "viem";
-
-type PremintV1Inputs = ExtractAbiFunction<
-  typeof preminterAbi,
-  "premintV1"
->["inputs"];
-
-type PremintV1HashDataTypes = AbiParametersToPrimitiveTypes<PremintV1Inputs>;
-
-export type ContractCreationConfig = PremintV1HashDataTypes[0];
-
-export type PremintConfigV1 = PremintV1HashDataTypes[1];
-export type TokenCreationConfigV1 = PremintConfigV1["tokenConfig"];
-
-export type MintArguments = PremintV1HashDataTypes[4];
-
-type PremintV2Inputs = ExtractAbiFunction<
-  typeof preminterAbi,
-  "premintV2"
->["inputs"];
-
-type PremintV2HashDataTypes = AbiParametersToPrimitiveTypes<PremintV2Inputs>;
-
-export type PremintConfigV2 = PremintV2HashDataTypes[1];
-export type TokenCreationConfigV2 = PremintConfigV2["tokenConfig"];
-
-const v1Types = {
-  CreatorAttribution: [
-    { name: "tokenConfig", type: "TokenCreationConfig" },
-    // unique id scoped to the contract and token to create.
-    // ensure that a signature can be replaced, as long as the replacement
-    // has the same uid, and a newer version.
-    { name: "uid", type: "uint32" },
-    { name: "version", type: "uint32" },
-    // if this update should result in the signature being deleted.
-    { name: "deleted", type: "bool" },
-  ],
-  TokenCreationConfig: [
-    { name: "tokenURI", type: "string" },
-    { name: "maxSupply", type: "uint256" },
-    { name: "maxTokensPerAddress", type: "uint64" },
-    { name: "pricePerToken", type: "uint96" },
-    { name: "mintStart", type: "uint64" },
-    { name: "mintDuration", type: "uint64" },
-    { name: "royaltyMintSchedule", type: "uint32" },
-    { name: "royaltyBPS", type: "uint32" },
-    { name: "royaltyRecipient", type: "address" },
-    { name: "fixedPriceMinter", type: "address" },
-  ],
-} as const;
-
-const v2Types = {
-  CreatorAttribution: [
-    { name: "tokenConfig", type: "TokenCreationConfig" },
-    // unique id scoped to the contract and token to create.
-    // ensure that a signature can be replaced, as long as the replacement
-    // has the same uid, and a newer version.
-    { name: "uid", type: "uint32" },
-    { name: "version", type: "uint32" },
-    // if this update should result in the signature being deleted.
-    { name: "deleted", type: "bool" },
-  ],
-  TokenCreationConfig: [
-    { name: "tokenURI", type: "string" },
-    { name: "maxSupply", type: "uint256" },
-    { name: "maxTokensPerAddress", type: "uint64" },
-    { name: "pricePerToken", type: "uint96" },
-    { name: "mintStart", type: "uint64" },
-    { name: "mintDuration", type: "uint64" },
-    { name: "royaltyBPS", type: "uint32" },
-    { name: "payoutRecipient", type: "address" },
-    { name: "fixedPriceMinter", type: "address" },
-    { name: "createReferral", type: "address" },
-  ],
-} as const;
-
-export const PreminterDomain = "Preminter";
-
-type PremintConfigVersion = "1" | "2";
-
-export const PremintConfigVersion = {
-  V1: "1",
-  V2: "2",
-} as const;
-
-type PremintConfigForVersion<T extends PremintConfigVersion> = T extends "1"
-  ? PremintConfigV1
-  : PremintConfigV2;
-
-type PremintConfigWithVersion<T extends PremintConfigVersion> = {
-  premintConfig: PremintConfigForVersion<T>;
-  premintConfigVersion: T;
-};
-
-export type PremintConfigAndVersion =
-  | PremintConfigWithVersion<"1">
-  | PremintConfigWithVersion<"2">;
+import {
+  ContractCreationConfig,
+  PremintConfigAndVersion,
+  PremintConfigV1,
+  PremintConfigV2,
+  PremintConfigVersion,
+  PreminterDomain,
+  v1Types,
+  v2Types,
+} from "./contract-types";
 
 export const getPremintExecutorAddress = () =>
   zoraCreator1155PremintExecutorImplAddress[999];
