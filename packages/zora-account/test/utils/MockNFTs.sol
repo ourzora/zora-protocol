d69d04513d832f1ff7b1c17afd9128af438cdac4
Rohan Kulkarni <kulkarohan@gmail.com>
2023-12-11 16:10:14 -0500

    refactor: import OZ nfts

--- packages/zora-account/test/utils/MockNFTs.sol
+++ packages/zora-account/test/utils/MockNFTs.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ERC721} from "./ERC721.sol";
-import {ERC1155} from "./ERC1155.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MockERC721 is ERC721 {
     address public creator;
