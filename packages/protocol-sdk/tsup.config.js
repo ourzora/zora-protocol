b2e57cc2a2721f5bed2d95261d8beda44f6bedb4
Dan Oved <stangogh@gmail.com>
2024-01-09 10:11:31 -0800

    Exclude test files from output

--- packages/protocol-sdk/tsup.config.js
+++ packages/protocol-sdk/tsup.config.js
@@ -4,7 +4,8 @@ export default defineConfig({
   entry: ["src/index.ts"],
   sourcemap: true,
   clean: true,
+  tsconfig: "tsconfig.build.json",
   dts: false,
   format: ["cjs", "esm"],
-  onSuccess: "tsc --project tsconfig.types.json --emitDeclarationOnly --declaration --declarationMap",
+  onSuccess: "tsc --project tsconfig.build.json --emitDeclarationOnly --declaration --declarationMap",
 });
