1904b043dc98c6e2d6d867a3990090f9bd7cb631
Iain Nash <me@iain.in>
2025-03-12 22:59:53 -0400

    Update UpgradeCoinImpl.sol

--- packages/coins/script/UpgradeCoinImpl.sol
+++ packages/coins/script/UpgradeCoinImpl.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 import {ProxyDeployerScript, DeterministicDeployerAndCaller} from "@zoralabs/shared-contracts/deployment/ProxyDeployerScript.sol";
 import {CoinsDeployerBase} from "./CoinsDeployerBase.sol";
 
-contract DeployScript is CoinsDeployerBase {
+contract UpgradeCoinImpl is CoinsDeployerBase {
     function run() public {
         CoinsDeployment memory deployment = readDeployment();
 
