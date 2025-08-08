8b85ab9449521187e966cac344f2168a5d459b73
Dan Oved <stangogh@gmail.com>
2025-08-08 14:10:53 -0700

    Removing some unused code (#1352)
    Removing some unused code, as pointed out in the Cantina review.
    https://cantina.xyz/code/e4fa76c6-c969-462c-9676-e7d4d60fc1dd/findings?severity=informational&finding=11

---
+++ .changeset/afraid-crabs-bow.md
@@ -0,0 +1,5 @@
+---
+"@zoralabs/coins": patch
+---
+
+Removed some unused constants
--- packages/coins/src/libs/MarketConstants.sol
+++ packages/coins/src/libs/MarketConstants.sol
@@ -6,20 +6,10 @@ library MarketConstants {
     uint256 constant WAD = 1e18;
 
     /// @notice The LP fee
-    /// @dev 10000 basis points = 1%
-    uint24 internal constant LP_FEE = 10000;
-
-    /// @notice The LP fee
     /// @dev 30000 basis points = 3%
     uint24 internal constant LP_FEE_V4 = 30000;
 
     /// @notice The spacing for 1% pools
     /// @dev 200 ticks
     int24 internal constant TICK_SPACING = 200;
-
-    /// @notice The minimum lower tick for legacy single LP WETH pools
-    int24 internal constant LP_TICK_LOWER_WETH = -208200;
-
-    /// @notice The upper tick for legacy single LP WETH pools
-    int24 internal constant LP_TICK_UPPER = 887200;
 }
